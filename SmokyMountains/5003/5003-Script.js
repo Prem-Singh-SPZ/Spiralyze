5d43e3aa7d951a3ac5f7e1b4237f3a98565dbf96
Prem Singh <prem@spiralyze.com>
2022-12-14 20:59:28 +0530

    5004 smockeymountains

--- SmokyMountains/5003/5003-Script.js
+++ SmokyMountains/5003/5003-Script.js
@@ -1,51 +1,148 @@
+(function () {
+    var jQueryInterval = setInterval(function () {
+        if (typeof jQuery != 'undefined') {
+            clearInterval(jQueryInterval);
+            checkAvailable();
+            jQuery(document).ready(function () {
+                if (!jQuery("body").hasClass("spz_5003")) {
+                    var cookieName = "5003_Sticky_Filter"; var cookieValue = "1"; var myDate = new Date(); myDate.setDate(myDate.getDate() + 30);
+                    document.cookie = cookieName + "=" + cookieValue + ";expires=" + myDate;
+                }
+            });
+        }
+    });
+    function checkAvailable() {
+        var checkLoad = setInterval(function () {
+            if (jQuery(".vms-filter__container").length > 0) {
+                clearInterval(checkLoad);
+                jQuery("body").addClass("spz_5003");
 
-var jQueryInterval = setInterval(function () {
-    if (typeof jQuery != 'undefined') {
-        clearInterval(jQueryInterval);
-        checkAvailable();
-        jQuery(document).ready(function () {
-            if (!jQuery("body").hasClass("spz_5003")) {
-                var cookieName = "5003_Sticky_Filter"; var cookieValue = "1"; var myDate = new Date(); myDate.setDate(myDate.getDate() + 30);
-                document.cookie = cookieName + "=" + cookieValue + ";expires=" + myDate;
+                //Header scroll changes
+                document.querySelector("#main .header.header--fluid").classList.remove('header--sticky');
+                headerScrolled();
+                jQuery(window).scroll(function () {
+                    headerScrolled();
+                });
+
+                //Filter changes
+                const params = new Proxy(new URLSearchParams(window.location.search), {
+                    get: (searchParams, prop) => searchParams.get(prop),
+                });
+
+                let checkInDate = formatDate(params.checkin);
+                var checkOutDate = formatDate(params.checkout);
+                let guests = params.guests;
+                let beds = params.beds;
+                const today = new Date();
+                if (navigator.userAgent.toLowerCase().indexOf('chrome/') == -1 && navigator.userAgent.toLowerCase().indexOf('safari/') > -1) {
+                    document.querySelector('body').classList.add('safari')
+                    var cotD = new Date(checkOutDate);
+                }
+                else {
+                    var cotD = new Date("'" + checkOutDate + "'");
+                }
+                if (window.innerWidth > 1288) {
+
+                    if (params.smc) {
+                        var locations = params.smc.split('|').length;
+                        addValue(locations, '.vms-filter__container .vms-filter-item:nth-child(5) button');
+                    }
+                    if (params.sma) {
+                        var amenities = params.sma.split('|').length;
+                        addValue(amenities, '.vms-filter__container .vms-filter-item:nth-child(4) button');
+                    }
+                    if (params.smt) {
+                        var type = params.smt.split('|').length;
+                        if (type == 1) {
+                            document.querySelector('.vms-filter__container .vms-filter-item:nth-child(6) button span').innerHTML = document.querySelector('.vms-filter__container .vms-filter-item:nth-child(6) button span').textContent + ':<span class="spz-filter-value"> ' + params.smt + '</span>';
+                        }
+                        else {
+                            document.querySelector('.vms-filter__container .vms-filter-item:nth-child(6) button').insertAdjacentHTML('beforeend', '<span class="spz-filter-value-index"> ' + type + '</span>');
+                        }
+                        document.querySelector('.vms-filter__container .vms-filter-item:nth-child(6) button').classList.add('active');
+                    }
+                    if (guests) {
+                        addCount(guests, '.vms-filter__container .vms-filter-item:nth-child(2) button');
+                    }
+                    if (beds) {
+                        addCount(beds, '.vms-filter__container .vms-filter-item:nth-child(3) button');
+                    }
+
+                    if (cotD > today) {
+                        document.querySelector('.vms-filter-item--dates button span').innerHTML = document.querySelector('.vms-filter-item--dates button span').textContent + ':<span class="spz-filter-value"> ' + checkInDate + ' &mdash; ' + checkOutDate + '</span>';
+                        document.querySelector('.vms-filter-item--dates button').classList.add('active');
+                    }
+
+                    //Reset All
+                    if ((cotD > today) || guests || beds || locations || amenities || type) {
+                        document.querySelector('.vms-filter-sort').insertAdjacentHTML('beforebegin', `<div class="vms-filter-item vms-reset-all">
+                <a href="https://smokymountains.com/search/">
+                    <img class="vms-filter-item-all__arrow vms-filter-item-all__cross vms-filter-item-all__cross-normal" src="//res.cloudinary.com/spiralyze/image/upload/v1670928151/SmokyMountains/5003/close-all.svg" alt="Close-all">
+                    <img class="vms-filter-item-all__arrow vms-filter-item-all__cross vms-filter-item-all__cross-hover" src="//res.cloudinary.com/spiralyze/image/upload/v1670909229/SmokyMountains/5003/close-all-hover.svg" alt="Close-all">
+                    <span class="vms-filter-item-all__title">Reset all</span></a>
+              </div>`)
+                    }
+                }
+
+                else {
+                    if (cotD > today) {
+                        document.querySelector('.vms-filter-item--dates button').classList.add('active');
+                    }
+                }
+                document.querySelector('#vms-filter-item-all').insertAdjacentHTML('afterbegin', '<img class=" vms-filter-item-all__filter" src="//res.cloudinary.com/spiralyze/image/upload/v1670854655/SmokyMountains/5003/white-filter.svg" alt="All-filter">')
             }
         });
     }
-});
-function checkAvailable() {
-    var checkLoad = setInterval(function () {
-        if (jQuery(".vms-filter__container").length > 0) {
-            clearInterval(checkLoad);
-            jQuery("body").addClass("spz_5003");
-            console.log('i m here')
-            document.querySelector("#main .header.header--fluid").classList.remove('header--sticky');
-            const params = new Proxy(new URLSearchParams(window.location.search), {
-                get: (searchParams, prop) => searchParams.get(prop),
-                getAll: (searchParams, prop) => console.log(searchParams.entries()),
-            });
 
-            let checkInDate = formatDate(params.checkin);
-            let checkOutDate = formatDate(params.checkout);
-            let guests = params.guests;
-            let beds = params.beds;
-            let locations = params.smc.split('|').length;
-            let ameneties = params.sma.split('|').length;
-            let type = params.smt;
-            console.log(checkInDate, checkOutDate, guests , beds , locations , ameneties , type);
+
+    //Generic code
+    let lastUrl = location.href;
+    new MutationObserver(() => {
+        const url = location.href;
+        if (url !== lastUrl) {
+            lastUrl = url;
+            jQuery("body").removeClass("spz_5003");
+            checkAvailable();
         }
-    });
-}
-let lastUrl = location.href;
-new MutationObserver(() => {
-    const url = location.href;
-    if (url !== lastUrl) {
-        lastUrl = url;
-        jQuery("body").removeClass("spz_5003");
-        checkAvailable();
-    }
-}).observe(document, { subtree: true, childList: true });
+    }).observe(document, { subtree: true, childList: true });
 
-function formatDate(inputDate) {
-    const date = new Date(inputDate);
-    const options = { year: 'numeric', month: 'short', day: 'numeric' };
-    return date.toLocaleDateString("en-US", options);
-}
+    //Input fields updates
+    function addValue(field, el) {
+        document.querySelector(el).insertAdjacentHTML('beforeend', '<span class="spz-filter-value-index"> ' + field + '</span>');
+        document.querySelector(el).classList.add('active');
+    }
+    function addCount(field, el) {
+        document.querySelector(el + ' span').innerHTML = document.querySelector(el + ' span').textContent + ':<span class="spz-filter-value"> ' + field + '</span>';
+        document.querySelector(el).classList.add('active');
+    }
+
+    //Format date
+    function formatDate(inputDate) {
+        const date = new Date(inputDate);
+        const options = { year: 'numeric', month: 'short', day: 'numeric' };
+        return date.toLocaleDateString("en-US", options);
+    }
+
+    //check if upper header is available
+    function isInViewport(el) {
+        const rect = el.getBoundingClientRect();
+        return (
+            rect.top >= 0 &&
+            rect.left >= 0 &&
+            rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+            rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+        );
+    }
+
+    //Lower header scroll
+    function headerScrolled() {
+        let upperHeader = document.querySelector("#main .header.header--fluid");
+        var scroll = jQuery(window).scrollTop();
+        isInViewport(upperHeader);
+        if (scroll >= 72) {
+            jQuery(".vms-filter").addClass("darkHeader");
+        } else {
+            jQuery(".vms-filter").removeClass("darkHeader");
+        }
+    }
+})()
---
+++ SmokyMountains/5004/5004-Script.js
@@ -0,0 +1,146 @@
+(function () {
+    let propCardHTML = '';
+    getData();
+
+    document.querySelector('head').insertAdjacentHTML("beforeend", `
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.min.css" integrity="sha512-yHknP1/AwR+yx26cB1y0cjvQUMvEa2PFzt1c9LlS4pRQ5NOTZFWbhBig+X9G9eYW/8m0/4OXNx8pxJ6z57x0dw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick-theme.min.css" integrity="sha512-17EgCFERpgZKcm0j0fEq1YCJuyAWdz9KUtv1EjVuaOz8pDnh/0nZxmU6BBXwaaxqoi9PQXnRWqlcDB027hgv9A==" crossorigin="anonymous" referrerpolicy="no-referrer" />`);
+
+    var jQueryInterval = setInterval(function () {
+        if (typeof jQuery != 'undefined') {
+            clearInterval(jQueryInterval);
+            const slickLib = document.createElement('script');
+            slickLib.src = 'https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.min.js';
+            document.head.appendChild(slickLib);
+
+            jQuery(document).ready(function () {
+                if (!jQuery("body").hasClass("spz_5004")) {
+                    var cookieName = "5004-Featured-Properties"; var cookieValue = "1"; var myDate = new Date(); myDate.setDate(myDate.getDate() + 30);
+                    document.cookie = cookieName + "=" + cookieValue + ";expires=" + myDate;
+                    checkAvailable();
+                }
+            });
+        }
+    });
+
+    function checkAvailable() {
+        var checkLoad = setInterval(function () {
+            if (jQuery("#results-container").find('.vms-results-item')) {
+                jQuery("body").addClass("spz_5004");
+                console.log('Test is running..');
+                document.querySelector('#results-container').insertAdjacentHTML('beforebegin', `<div class="featured-products-spz vms-results__container"></div>`);
+                document.querySelector('#results-container').insertAdjacentHTML('beforebegin', `<div class="featured-products-dummy" style="display:none;"></div>`);
+                document.querySelector('.featured-products-dummy').insertAdjacentHTML('afterbegin', propCardHTML.innerHTML);
+                document.querySelectorAll('.featured-products-dummy .vms-results-item').forEach(function (element, index) {
+                    if (index < 10) {
+                        document.querySelector('.featured-products-spz').insertAdjacentHTML('beforeend', element.outerHTML);
+                    }
+                })
+                document.querySelectorAll('.featured-products-spz .vms-results-item').forEach(function (element, index) {
+                    element.classList.add('spz-prop-card');
+                    element.classList.remove('vms-results-item');
+                    let img = element.querySelector('.vms-results-item__thumb');
+                    let summary = element.querySelector('.vms-results-item__summary');
+                    let title = element.querySelector('.vms-results-item__title');
+                    let meta = element.querySelector('.vms-results-item__meta');
+                    element.insertAdjacentHTML('afterbegin', `<div class='spz-card-img'>` + img.outerHTML + `</div>`);
+                    element.insertAdjacentHTML('beforeend', `<div class='spz-card-content'>` + title.outerHTML + summary.outerHTML + meta.outerHTML + `</div>`);
+                    img.remove();
+                    summary.remove();
+                    title.remove();
+                    meta.remove();
+                })
+                document.querySelectorAll('.featured-products-spz .spz-prop-card').forEach(function (v, i) {
+                    v.querySelector('.spz-card-content .vms-results-item__meta .vms-results-item__price').innerHTML = v.querySelector('.spz-card-content .vms-results-item__meta .vms-results-item__price').innerHTML.replace('/Night ', ' night');
+                    let ratings = v.querySelector('.spz-card-content .vms-results-item__meta .vms-results-item-rating .vms-results-item-rating__title').textContent.split('/');
+                    v.querySelector('.spz-card-content .vms-results-item__meta .vms-results-item-rating .vms-results-item-rating__title').textContent = ratings[0];
+                });
+
+
+                if (document.querySelector('.featured-products-spz') && !document.querySelector('.featured-products-spz.slick-initialized .slick-track')) {
+                    jQuery('.featured-products-spz').slick({
+                        arrows: true,
+                        dots: true,
+                        infinite: false,
+                        speed: 1000,
+                        // autoplay: true,
+                        // autoplaySpeed: 2000,
+                        slidesToShow: 2.755,
+                        // centerMode: false,
+                        slidesToScroll: 1,
+                        prevArrow: '<button class="slide_arrow prev-arrow"><img src="//res.cloudinary.com/spiralyze/image/upload/v1671021183/SmokyMountains/5004/assets/Arrow-left.svg" alt="Previous" /></button>',
+                        nextArrow: '<button class="slide_arrow next-arrow"><img src="//res.cloudinary.com/spiralyze/image/upload/v1671021183/SmokyMountains/5004/assets/Arrow-right.svg" alt="Next" /></button>',
+
+                        responsive: [{
+                            breakpoint: 1366,
+                            settings: {
+                                slidesToShow: 2.25,
+                            }
+                        }, {
+                            breakpoint: 1024,
+                            settings: {
+                                slidesToShow: 1.5,
+                            }
+                        }, {
+                            breakpoint: 600,
+                            settings: {
+                                slidesToShow: 1.25,
+                            }
+                        }, {
+                            breakpoint: 300,
+                            settings: "unslick" // destroys slick
+                        }]
+                    });
+                }
+
+                if (document.querySelector('.featured-products-spz.slick-initialized .slick-track .spz-prop-card')) {
+                    clearInterval(checkLoad);
+                    document.querySelector('.featured-products-spz.slick-initialized .next-arrow').onclick = function () {
+                        if (document.querySelector('.featured-products-spz.slick-initialized .next-arrow').classList.contains('slick-disabled')) {
+                            document.querySelector('.featured-products-spz.slick-initialized .slick-list').classList.add('right-shadow-disable');
+                        }
+                    };
+                    document.querySelector('.featured-products-spz.slick-initialized .prev-arrow').onclick = function () {
+                        if (document.querySelector('.featured-products-spz.slick-initialized .slick-list').classList.contains('right-shadow-disable')) {
+                            document.querySelector('.featured-products-spz.slick-initialized .slick-list').classList.remove('right-shadow-disable');
+                        }
+                    };
+                    document.querySelector('.featured-products-spz.slick-initialized .slick-dots li:last-child').onclick = function () {
+                        if (document.querySelector('.featured-products-spz.slick-initialized .next-arrow').classList.contains('slick-disabled')) {
+                            document.querySelector('.featured-products-spz.slick-initialized .slick-list').classList.add('right-shadow-disable');
+                        }
+                    };
+                    jQuery(".featured-products-spz.slick-initialized .slick-dots li:not(:last-child)").click(function () {
+                        if (document.querySelector('.featured-products-spz.slick-initialized .slick-list').classList.contains('right-shadow-disable')) {
+                            document.querySelector('.featured-products-spz.slick-initialized .slick-list').classList.remove('right-shadow-disable');
+                        }
+                    });
+                }
+            }
+        }, 100);
+    }
+    let lastUrl = location.href;
+    new MutationObserver(() => {
+        const url = location.href;
+        if (url !== lastUrl) {
+            lastUrl = url;
+            jQuery("body").removeClass("spz_5004");
+            checkAvailable();
+        }
+    }).observe(document, { subtree: true, childList: true });
+
+    function getData() {
+        fetch('https://smokymountains.com/search/?srt=RHF').then(function (response) {
+            // The API call was successful!
+            return response.text();
+        }).then(function (html) {
+            // Convert the HTML string into a document object
+            var parser = new DOMParser();
+            var doc = parser.parseFromString(html, 'text/html');
+            propCardHTML = doc.querySelector('#results-container');
+        }).catch(function (err) {
+            // There was an error
+            console.warn('Something went wrong.', err);
+        });
+    }
+})()
