e0e88b204a4688937375c7f6b2fbcc2a64ce20cd
Prem-Singh-SPZ <prem@spiralyze.com>
2023-05-22 20:05:06 +0530

    22 may

--- Coveo/18001/18001-Script.js
+++ Coveo/18001/18001-Script.js
@@ -1,13 +1,12 @@
 
 jQuery(function ($) {
     $(document).ready(function () {
-        console.log('18001 Test is ready..')
         $('.assesment_cta').on('click', function (e) {
-            $('html, body').animate( {scrollTop: $($('.form-container-white')).offset().top - 50, }, 500, 'linear' );
+            $('html, body').animate({ scrollTop: $($('.form-container-white')).offset().top - 50, }, 500, 'linear');
         });
 
-        if(600 < window.innerWidth < 1300){
-            $('#lp-pom-image-1003 img').attr('src','https://res.cloudinary.com/spiralyze/image/upload/v1683828622/coveo/18001/768_logos.webp');
+        if ((window.innerWidth > 600) && (window.innerWidth < 1300)) {
+            $('#lp-pom-image-1003 .lp-pom-image-container').html('<img src="https://res.cloudinary.com/spiralyze/image/upload/v1683828622/coveo/18001/768_logos.webp" alt="Client Logos">');
         }
     });
 });
