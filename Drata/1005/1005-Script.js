bcf5cc7129307d7f92bc43b7e3a6c928af1de378
Prem-Singh-SPZ <prem@spiralyze.com>
2023-06-16 20:33:41 +0530

    16 Jun

--- Drata/1005/1005-Script.js
+++ Drata/1005/1005-Script.js
@@ -26,8 +26,8 @@
         appendInputLabel();
 
         // Set input label
-        document.querySelector('[name="source__inbound_demo_"] + .hs-label-spz').innerHTML = 'How did you hear about Drata?*';
-        document.querySelector('[name="source__inbound_demo_"]').setAttribute('placeholder', 'How did you hear about Drata?*');
+        document.querySelector('[name="source__inbound_demo_"] + .hs-label-spz').innerHTML = 'How did you hear about us?*';
+        document.querySelector('[name="source__inbound_demo_"]').setAttribute('placeholder', 'How did you hear about us?*');
         document.querySelector('label#label-demo_product_of_interest-429140d2-bd90-4a8b-a561-5d732c9bd514 + .hs-field-desc').innerHTML = 'What product(s) are you interested in?';
         // Set SOC-2 checkbox checked
         document.querySelector('[name="demo_product_of_interest"]').setAttribute('checked', 'checked');
