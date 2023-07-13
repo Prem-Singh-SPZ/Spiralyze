16f4c79ee1acc6e7ee926f14ab2d5325a43c81df
Prem-Singh-SPZ <prem@spiralyze.com>
2023-07-13 16:02:10 +0530

    13 jul prog

--- Taboola/with-num-br-c/1046-br-c.js
+++ Taboola/with-num-br-c/1046-br-c.js
@@ -108,7 +108,7 @@ const formInputId = getFormInputId();
 $(`#element-647 form input#${formInputId}-0, #element-647 form label[for="${formInputId}-0"]`).wrapAll('<div class="input_field field-0"></div>');
 $(`#element-647 form input#${formInputId}-1, #element-647 form label[for="${formInputId}-1"]`).wrapAll('<div class="input_field field-1"></div>');
 $(`#element-647 form input#${formInputId}-2, #element-647 form label[for="${formInputId}-2"]`).wrapAll('<div class="input_field field-2"></div>');
-$(`#element-647 form input#${formInputId}-3, #element-647 form label[for="${formInputId}-3"]`).wrapAll('<div class="input_field field-3"></div>');
+$(`#element-647 form input#${formInputId}-3, #element-647 form label[for="${formInputId}-3"]`).wrapAll('<div class="input_field field-3 invalid_phone"></div>');
 $(`#element-647 form input#${formInputId}-4, #element-647 form label[for="${formInputId}-4"]`).wrapAll('<div class="input_field field-4"></div>');
 $(`#element-647 form input#${formInputId}-5, #element-647 form label[for="${formInputId}-5"]`).wrapAll('<div class="input_field field-5"></div>');
 $(`#element-647 form label[for="${formInputId}-6"] , #element-647 form .form-block-select select#${formInputId}-6`).wrapAll('<div class="input_field form-block-select field-6"></div>');
@@ -120,19 +120,26 @@ $(`#element-647 form label[for="${formInputId}-7"] , #element-647 form .form-blo
 $(`#element-647 form select#${formInputId}-6 option.hidden`).text('Company Size');
 $(`#element-647 form select#${formInputId}-7 option.hidden`).text('Company Type');
 
-var selector = '#element-647 form input, #element-647 form select';
+
+// All fields except phone number starts
+var selector = '#element-647 form input:not([name="Mobile Phone (optional)"]), #element-647 form select';
 
 $(document).on('focus', selector, function () {
     $(this).closest('.input_field').addClass('active typing');
 });
 $(document).on('blur focusout keyup', selector, function (e) {
     if ($(this).val() == null || $(this).val() == '') {
-        $(this).closest('.input_field').removeClass('active filled');
+        $(this).closest('.input_field').removeClass('active filled fill');
         $(this).closest('.input_field').addClass('error');
     } else {
         $(this).closest('.input_field').addClass('active filled');
-        $(this).closest('.input_field').removeClass('error');
+        if(jQuery(this).hasClass('error')){
+          $(this).closest('.input_field').addClass('error');
+        }else{
+          $(this).closest('.input_field').removeClass('error');
+        }
     }
+    
 });
 
 $(document).on('focusout', selector, function (e) {
@@ -158,19 +165,66 @@ jQuery(document).on('keyup', 'input#Email', function (e) {
     }
 });
 
-jQuery(document).on('keyup', 'input#Phone', function (e) {
-    var phonevalue = jQuery(this).val();
-    var regex = /^[+]*[(]{0,1}[0-9]{1,4}[)]{0,1}[-\s\./0-9]*$/;
-    if (!regex.test(phonevalue) || phonevalue.length < 10 || phonevalue.length > 12) {
-        $(this).closest('.input_field ').addClass('invalid_phone');
-        return regex.test(phonevalue);
+// Only phone number
+jQuery(document).on('keyup', `input#${formInputId}-3`, function (e) {
+    let phoneInputValues = jQuery(this).val();
+    let phoneRegex = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{3,6}$/im;
+    if (!phoneRegex.test(phoneInputValues)) {
+        $(this).closest('.input_field').addClass('invalid_phone');
+        if ($('body').hasClass('fill-phone')) {
+            $('body').removeClass('fill-phone');
+        }
+        jQuery(this).next('.error_msg').show();
+        jQuery(this).next('.error_text.phone').show();
+        return phoneRegex.test(phoneInputValues);
     } else {
-        $(this).closest('.input_field ').removeClass('invalid_phone');
+        $(this).closest('.input_field').removeClass('invalid_phone');
+        $('body').addClass('fill-phone');
+        jQuery('.error_text.phone').hide();
+        jQuery('.ao-clicar .hidden-checkbox').val('no');
+
+        telCheckbox();
     }
 });
 
+// only phone number
+$(document).on('focus', '#element-647 form input[name="Mobile Phone (optional)"]', function () {
+    $(this).closest('.input_field').addClass('active typing');
+});
+
+// only phone number
+$(document).on('blur focusout keyup', '#element-647 form input[name="Mobile Phone (optional)"]', function (e) {
+    if ($(this).val() == null || $(this).val() == '') {
+        $(this).closest('.input_field').removeClass('active filled fill');
+        // $(this).closest('.input_field').addClass('error');
+    } else {
+        $(this).closest('.input_field').addClass('active filled');
+        $(this).closest('.input_field').removeClass('error fill');
+    }
+});
+
+// Only phone number keydown event
+const phoneNoEl = document.querySelector('#element-647 .input_field.field-3 input');
+phoneNoEl.setAttribute('maxlength', 12)
+phoneNoEl.addEventListener('keydown', function (event) {
+    var digitPeriodRegExp = new RegExp('\\d|\\.');
+
+    if (event.ctrlKey // (A)
+        || event.altKey // (A)
+        || typeof event.key !== 'string' // (B)
+        || event.key.length !== 1) { // (C)
+        return;
+    }
+
+    if (!digitPeriodRegExp.test(event.key)) {
+        event.preventDefault();
+    }
+
+}, false);
 
 
+
+// All input fields blur event
 var required_filed = "#element-647 form input";
 
 jQuery(document).on('focusout blur', '#element-647 form input', function () {
@@ -182,52 +236,93 @@ jQuery(document).on('focusout blur', '#element-647 form input', function () {
     } else {
         jQuery(this).parents('.input_field').removeClass('fill');
     }
-}
+});
 
-);
-
-jQuery(document).on('focusout blur', required_filed, function () {
+// All inputs except phone number   
+jQuery(document).on('focusout blur', required_filed + ':not([name="Mobile Phone (optional)"])', function () {
     var input_val = jQuery(this).val();
     jQuery(this).parent('.input_field').removeClass('active typing');
-
     if (input_val == '' || input_val == undefined || input_val == NaN || input_val == null || input_val.trim().length < 1) {
         jQuery(this).parent('.input_field').removeClass('fill').addClass('error');
     } else {
-        jQuery(this).parent('.input_field').addClass('fill').removeClass('error');
+        if (jQuery(this).hasClass('error')) {
+            jQuery(this).parent('.input_field').removeClass('fill').addClass('error');
+        } else {
+            jQuery(this).parent('.input_field').addClass('fill').removeClass('error');
+        }
+
     }
+});
 
-    //convert_button_color();
-}
 
-);
+// Only phone number
+jQuery(document).on('focusout blur', required_filed + '[name="Mobile Phone (optional)"]', function () {
+    var input_val = jQuery(this).val();
+    jQuery(this).parent('.input_field').removeClass('active typing');
+    if (input_val == '' || input_val == undefined || input_val.checked) {
+        jQuery('.ao-clicar .form-multiple-input').removeClass('required user-invalid');
+        jQuery('.error_text.phone').hide();
+        formButton.removeAttribute('disabled', '');
+        formButton.style.opacity = '1';
+        formButton.style.cursor = 'pointer';
+        jQuery(this).parents('.input_field').removeClass('fill');
+    } else {
+        jQuery('.ao-clicar .hidden-checkbox').val('no')
+        jQuery('.ao-clicar .form-multiple-input').attr('required', 'required').addClass('required user-invalid');
+        formButton.setAttribute('disabled', '');
+        formButton.style.opacity = '0.5';
+        formButton.style.cursor = 'auto';
+        jQuery(this).parents('.input_field').removeClass('fill');
+        telCheckbox();
+    }
+});
 
 
 $('<div class="error_msg">\
     <h5 class="error_text">This field is required</h5>\
     <h5 class="error_text email">Must be valid email</h5>\
+    <h5 class="error_text phone">Must be valid phone</h5>\
 </div>').insertAfter('#element-647 form input');
 
 
 
 
-jQuery(`.input_field.field-2 input.${formInputId}-2`).keyup(function () {
+jQuery(`.input_field.field-2 input#${formInputId}-2`).keyup(function () {
 
-    var text = jQuery(`input.${formInputId}-2`).attr('class')
+    var text = jQuery(`input#${formInputId}-2`).attr('class')
     const myArray = text.split(" ");
     var word = myArray[3];
     if (word == 'user-invalid') {
-        jQuery(`input.${formInputId}-2`).next('.error_msg').show()
-        jQuery(`input.${formInputId}-2`).next('.error_msg').first('h5').find('h5.error_text').hide()
-        jQuery(`input.${formInputId}-2`).next('.error_msg').first('h5').find('h5.error_text.email').show()
+        jQuery(`input#${formInputId}-2`).next('.error_msg').show()
+        jQuery(`input#${formInputId}-2`).next('.error_msg').first('h5').find('h5.error_text').hide()
+        jQuery(`input#${formInputId}-2`).next('.error_msg').first('h5').find('h5.error_text.email').show()
 
     } else {
-        jQuery(`.input_field.field-2 input.${formInputId}-2`).next('.error_msg').hide()
+        jQuery(`.input_field.field-2 input#${formInputId}-2`).next('.error_msg').hide()
     }
 
 });
 
+// Form input phone number
+jQuery(`.input_field.field-3 input#${formInputId}-3`).keyup(function () {
+    if ($(this).parent().hasClass('invalid_phone')) {
+        jQuery(this).next('.error_msg').show()
+        jQuery(this).next('.error_msg').first('h5').find('h5.error_text').hide()
+        jQuery(this).next('.error_msg').first('h5').find('h5.error_text.email').hide()
+        jQuery(this).next('.error_msg').first('h5').find('h5.error_text.phone').show()
+        if ($('body').hasClass('fill-phone')) {
+            $('body').removeClass('fill-phone');
+        }
+    } else {
+        jQuery(`.input_field.field-3 input#${formInputId}-3`).next('.error_msg').hide()
+        $('body').addClass('fill-phone');
+    }
+});
+
+
 $('.form-block-checkbox').addClass('Iamnot')
-$(`input#${formInputId}-26-01`).change(function () {
+$(`#${formInputId}-26-0`).parents('.form-checkable-field').addClass('Iamnot-ips');
+$(`input#${formInputId}-26-0`).change(function () {
     if ($(this).prop("checked") == true) {
         $(this).closest('.form-block-checkbox').removeClass('Iamnot')
     } else {
@@ -236,25 +331,53 @@ $(`input#${formInputId}-26-01`).change(function () {
     }
 });
 
+$(`#${formInputId}-27-0`).parents('.form-checkable-field').addClass('ao-clicar');
+// on change checkbox element `input#${formInputId}-9-0` add 'user-invalid' class on itself
+// on change checkbox element `input#${formInputId}-9-0` remove 'user-invalid' class on itself
+// For phone number consent checkbox enable/disable form button
+setTimeout(() => {
+    document.querySelector(`input#${formInputId}-9-0`).addEventListener('change', function (e) {
+        if (e.target.checked) {
+            e.target.classList.remove('user-invalid');
+            formButton.removeAttribute('disabled', '');
+            formButton.style.opacity = '1';
+            formButton.style.cursor = 'pointer';
+        } else {
+            e.target.classList.add('user-invalid');
+            jQuery('.ao-clicar .hidden-checkbox').val('no');
+            formButton.setAttribute('disabled', '');
+            formButton.style.opacity = '0.5';
+            formButton.style.cursor = 'auto';
+        }
+    });
+}, 2000);
 
-var style = document.createElement('style');
+let style = document.createElement('style');
 style.innerHTML = '.error{border-color: #e85f54!important; }';
 document.getElementsByTagName('head')[0].appendChild(style);
 
 //check url validation
 function validateURL(string) {
-    var regex = /^((ftp|http|https):\/\/)?(www.)?(?!.*(ftp|http|https|www.))[a-zA-Z0-9_-]+(\.[a-zA-Z]+)+((\/)[\w#]+)*(\/\w+\?[a-zA-Z0-9_]+=\w+(&[a-zA-Z0-9_]+=\w+)*)?\/?$/i;
+    let regex = /^((ftp|http|https):\/\/)?(www.)?(?!.*(ftp|http|https|www.))[a-zA-Z0-9_-]+(\.[a-zA-Z]+)+((\/)[\w#]+)*(\/\w+\?[a-zA-Z0-9_]+=\w+(&[a-zA-Z0-9_]+=\w+)*)?\/?$/i;
     return regex.test(string);
 }
-var websiteInput = document.getElementById(`${formInputId}-3`);
+let websiteInput = document.getElementById(`${formInputId}-4`);
 
-var formButton = document.querySelector('[data-at="form-button"]');
+let formButton = document.querySelector('[data-at="form-button"]');
 websiteInput.addEventListener('keyup', function (e) {
     if (validateURL(websiteInput.value)) {
         websiteInput.classList.remove('error');
-        formButton.removeAttribute('disabled', '');
-        formButton.style.opacity = '1';
-        formButton.style.cursor = 'pointer';
+        // formButton.removeAttribute('disabled', '');
+        // formButton.style.opacity = '1';
+        // formButton.style.cursor = 'pointer';
+
+        if (jQuery('.ao-clicar .form-multiple-input').hasClass('user-invalid')) {
+            telCheckbox();
+        } else {
+            formButton.removeAttribute('disabled', '');
+            formButton.style.opacity = '1';
+            formButton.style.cursor = 'pointer';
+        }
 
     } else {
         websiteInput.classList.add('error');
@@ -270,4 +393,19 @@ function getFormInputId() {
     var firstInputId = document.querySelector('#element-647 form input').id;
     var firstInputId = firstInputId.slice(0, -2);
     return firstInputId;
+}
+
+
+
+// Prevent form submission if phone number consent checkbox is not checked
+function telCheckbox() {
+    var hasErrorInt = setInterval(() => {
+        const isValidPhone = jQuery('#element-647 form input[name="Mobile Phone (optional)"]').parent('.input_field').hasClass('invalid_phone');
+        if (!isValidPhone && jQuery('.ao-clicar .form-multiple-input').hasClass('user-invalid')) {
+            clearInterval(hasErrorInt);
+            formButton.setAttribute('disabled', '');
+            formButton.style.opacity = '0.5';
+            formButton.style.cursor = 'auto';
+        }
+    }, 50);
 }
