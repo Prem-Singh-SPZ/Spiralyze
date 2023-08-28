954b344635ef2b92941e106e79d31519641c4460
Prem-Singh-SPZ <prem@spiralyze.com>
2023-08-28 15:39:03 +0530

    28 Aug

--- Drata/3003/3003-Script.js
+++ Drata/3003/3003-Script.js
@@ -33,14 +33,14 @@ const blogHTML = `<div class="sticky-blog-section">
 <div class="blog-img">
     <picture>
         <source srcset="//res.cloudinary.com/spiralyze/image/upload/f_auto/drata/3003/img.webp" type="image/webp">
-        <source srcset="//res.cloudinary.com/spiralyze/image/upload/v1690989369/drata/3003/img.png" type="image/png"> <img src="https://res.cloudinary.com/spiralyze/image/upload/f_auto/drata/3003/img.webp" alt="Get Compliant 80% Faster With Drata"> </picture>
+        <source srcset="//res.cloudinary.com/spiralyze/image/upload/f_auto/drata/3003/img.png" type="image/png"> <img src="//res.cloudinary.com/spiralyze/image/upload/f_auto/drata/3003/img.webp" alt="Get Compliant 80% Faster With Drata"> </picture>
 </div>
 <div class="blog-container">
     <h6 class="blog-title">Get Compliant 80% Faster<br> With Drata</h6>
-    <div class="blog-rating"> <img class="g2-logo" src="https://res.cloudinary.com/spiralyze/image/upload/v1690989370/drata/3003/logo-g2.svg" alt="G2 Logo" /> <img class="star-ratings" src="https://res.cloudinary.com/spiralyze/image/upload/v1690989369/drata/3003/rating-4_9.svg" alt="Ratings" />
+    <div class="blog-rating"> <img class="g2-logo" src="//res.cloudinary.com/spiralyze/image/upload/v1690989370/drata/3003/logo-g2.svg" alt="G2 Logo" /> <img class="star-ratings" src="//res.cloudinary.com/spiralyze/image/upload/v1690989369/drata/3003/rating-4_9.svg" alt="Ratings" />
         <div class="ratings"> <span>4.9 </span> (461 reviews) </div>
     </div>
-    <div class="blog-cta"> <a href="Javascript:void(0)" class="hs-cta-primary trigger-demo-btn">Get a Demo <img src="https://res.cloudinary.com/spiralyze/image/upload/v1690987050/drata/3002/cta_arrow.svg" alt="Arrow" />
+    <div class="blog-cta"> <a href="Javascript:void(0)" class="hs-cta-primary trigger-demo-btn">Get a Demo <img src="//res.cloudinary.com/spiralyze/image/upload/v1690987050/drata/3002/cta_arrow.svg" alt="Arrow" />
             </a> </div>
 </div>
 </div>`;
