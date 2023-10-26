b04a0252af783fe3f679202e865d16856e55962e
Prem-Singh-SPZ <prem@spiralyze.com>
2023-10-26 16:35:40 +0530

    28003

--- Coveo/28003/28003-Script.js
+++ Coveo/28003/28003-Script.js
@@ -30,6 +30,15 @@ waitForElm('body').then((elm) => {
     //         }, 1000);
     //     }
     // });
+
+    if (location.href.indexOf('resources/ebooks/cio-cure-genai-headaches') > -1) {
+        waitForElm('.large[data-form-id="resources_details"] div[am-form-messages] div[data-cy="resource-document-success-message"]').then((elm) => {
+            setCookieForTimer('spz_form_submitted', 1, (30 * 24));
+        });
+        waitForElm('.form-container .card [data-cy="resource-cta"] [data-tracking="view-resource-cta"]').then((elm) => {
+            setCookieForTimer('spz_form_submitted', 1, (30 * 24));
+        });
+    }
 });
 
 function exitpopupconfig() {
@@ -40,14 +49,15 @@ function exitpopupconfig() {
         mouseY = evt.clientY;
         var myCookie = getCookie("spzcoveo28002exitpopup");
         let count = +(getCookie('spz_existing_user'));
-        if (mouseY <= topValue && myCookie == null && count < 4) {
+        let isFormSubmitted = getCookie('spz_form_submitted')
+        if (mouseY <= topValue && myCookie == null && count < 4 && isFormSubmitted == null) {
             var bodyTag = document.querySelector('body');
             if (!bodyTag.classList.contains('exitpopup28002displayed')) {
                 bodyTag.classList.add('exitpopup28002displayed');
                 document.querySelectorAll('html')[0].classList.add('spz-coveo-28002-popup-visible');
                 if (myCookie == null) {
                     document.cookie = `spzcoveo28002exitpopup=1; expires=${new Date(new Date().getTime() + 1000 * 60 * 60 * 24 * 365).toGMTString()}; path=/`;
-                    
+
                     let count = +(getCookie('spz_existing_user'));
 
                     if (getCookie('spz_existing_user')) {
@@ -66,7 +76,7 @@ function exitPopupInit() {
         <div class="spz-coveo-overlay">
             <div class="spz-coveo-popup">
                 <img src="//res.cloudinary.com/spiralyze/image/upload/f_auto/Coveo/28002/diamond-shape.png" alt="Diamond" class="spz-bg-diamond">
-                <img src="//res.cloudinary.com/spiralyze/image/upload/v1694609669/coveo/28002/desktop_image_1.svg" alt="Coveo Result" class="spz-result">
+                <img src="//res.cloudinary.com/spiralyze/image/upload/f_auto/coveo/28003/genaiheadaches_cover_1440_-_without_shadow.webp" alt="Coveo Result" class="spz-result">
                 <!-- close popup icon -->
                 <a class="spz-close">
                     <svg width="12" height="12" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M11.8334 1.34166L10.6584 0.166656L6.00008 4.82499L1.34175 0.166656L0.166748 1.34166L4.82508 5.99999L0.166748 10.6583L1.34175 11.8333L6.00008 7.17499L10.6584 11.8333L11.8334 10.6583L7.17508 5.99999L11.8334 1.34166Z" fill="#C5CACF"/></svg>    
@@ -76,11 +86,11 @@ function exitPopupInit() {
                         <img src="//res.cloudinary.com/spiralyze/image/upload/v1694609732/coveo/28002/coveo_horizontal_logo.svg" alt="Coveo Logo">
                     </div>
                     <div class="spz-coveo-popup-content">
-                        <h2 class="spz-coveo-popup-content-title">The world's first relevance <br>AI platform</h2>
+                        <h2 class="spz-coveo-popup-content-title">GenAI Headaches:<br><span>The Cure for CIOs</span></h2>
                         <div class="spz-coveo-popup-content-description">
-                            <p>Factually accurate, contextually relevant question answering. Enterprise-grade security. <br>Multiple content sources.</p>
+                            <p>Pressure is mounting to create Generative AI solutions – but cautions abound. Here&#8217;s how to meet your goals, while reducing your costs and risks.</p>
                         </div>
-                        <a href="https://www.coveo.com/en/contact/generative-ai" class="spz-coveo-popup-cta">Get a demo</a>
+                        <a href="https://www.coveo.com/en/resources/ebooks/cio-cure-genai-headaches" class="spz-coveo-popup-cta">Download the Ebook</a>
                         <img src="//res.cloudinary.com/spiralyze/image/upload/v1694755399/Coveo/28002/google-reviews.svg" alt="Google Review" class="spz-coveo-popup-greview">
                     </div>
                 </div>
@@ -152,16 +162,4 @@ function setCookieForTimer(cName, cValue, expHours) {
     date.setTime(date.getTime() + (expHours * 60 * 60 * 1000));
     const expires = "expires=" + date.toUTCString();
     document.cookie = cName + "=" + cValue + "; " + expires + "; path=/";
-}
-
-//Get a cookie
-function getCookie(cName) {
-    const name = cName + "=";
-    const cDecoded = decodeURIComponent(document.cookie); //to be careful
-    const cArr = cDecoded.split('; ');
-    let res;
-    cArr.forEach(val => {
-        if (val.indexOf(name) === 0) res = val.substring(name.length);
-    })
-    return res;
 }
