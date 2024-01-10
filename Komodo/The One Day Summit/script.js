6d6ac4655ecdbc888cdc9a598d9e7d477bae9c93
Prem-Singh-SPZ <prem@spiralyze.com>
2024-01-10 20:48:39 +0530

    hokomo

---
+++ Komodo/The One Day Summit/script.js
@@ -0,0 +1,10 @@
+$().ready(function () {
+    $('.slick-carousel').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: true,
+        // centerPadding: "0px",
+        dots: true,
+        infinite: false,
+    });
+});
