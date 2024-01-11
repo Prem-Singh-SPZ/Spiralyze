f9fd956d48ffec512915a120b2be49ff5de1ba0f
Prem-Singh-SPZ <prem@spiralyze.com>
2024-01-11 17:39:07 +0530

    3001

--- Komodo/The One Day Summit/script.js
+++ Komodo/The One Day Summit/script.js
@@ -1,10 +1,28 @@
 $().ready(function () {
     $('.slick-carousel').slick({
-        slidesToShow: 1,
+        slidesToShow: 1.3,
         slidesToScroll: 1,
         arrows: true,
         // centerPadding: "0px",
         dots: true,
         infinite: false,
+        variableWidth: true,
+        responsive: [
+            {
+                breakpoint: 1023,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 3,
+                }
+            },
+            {
+                breakpoint: 767,
+                settings: {
+                    slidesToShow: 1,
+                    arrows: false,
+                    slidesToScroll: 1
+                }
+            }
+        ]
     });
 });
