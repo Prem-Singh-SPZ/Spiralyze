b3ba53c5848646cd20a91f7f87d2b39d6d87a0c5
Prem-Singh-SPZ <prem@spiralyze.com>
2024-01-19 17:50:51 +0530

    fixes

--- Komodo/FAQ/script.js
+++ Komodo/FAQ/script.js
@@ -1,29 +1,12 @@
 $().ready(function () {
-    $('.slick-carousel').slick({
-        slidesToShow: 1.3,
-        slidesToScroll: 1,
-        arrows: true,
-        // centerPadding: "0px",
-        dots: true,
-        infinite: false,
-        variableWidth: true,
-        responsive: [
-            // {
-            //     breakpoint: 1023,
-            //     settings: {
-            //         slidesToShow: 1,
-            //         slidesToScroll: 1,
-            //     }
-            // },
-            {
-                breakpoint: 767,
-                settings: {
-                    slidesToShow: 1,
-                    arrows: false,
-                    slidesToScroll: 1,
-                    variableWidth: false,
-                }
-            }
-        ]
+    console.log('test')
+
+    document.addEventListener("DOMContentLoaded", (event) => {
+        window.addEventListener('click', function (el) {
+            el.target.classList.toggle('collapsed');
+            let id = el.target.getAttribute('data-bs-target');
+            this.document.querySelector(id).classList.toggle('show');
+        },);
     });
+
 });
