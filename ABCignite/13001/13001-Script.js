7d5d0793f8f205859e38ce161bc86532065e487e
Prem-Singh-SPZ <prem@spiralyze.com>
2024-01-23 13:50:08 +0530

    13001

--- ABCignite/13001/13001-Script.js
+++ ABCignite/13001/13001-Script.js
@@ -35,8 +35,10 @@ function urlCheck(url) {
     }
     if (isSameUrl(url, allblogsurlstring, true)) {
         init_13001();
+        init5009();
     }
 }
+
 
 function isSameUrl(currentUrl, specifiedUrl, includeQueryParams) {
     currentUrl = currentUrl.includes("#") ?
@@ -110,6 +112,83 @@ function init_13001() {
 
         });
     }
+}
+
+
+function init5009() {
+    var bodyEle = document.querySelector('body');
+    if (!bodyEle.classList.contains('spz-5010')) {
+        bodyEle.classList.add('spz-5010');
+        bodyEle.classList.add('spz-5011');
+
+        var link = document.createElement('link');
+        link.type = 'text/css';
+        link.rel = 'stylesheet';
+        link.href = '//res.cloudinary.com/spiralyze/raw/upload/f_auto/ABCFitnessIgnite/5010/updated_css/5010-Style.css';
+        document.head.appendChild(link);
+
+        var link5011 = document.createElement('link');
+        link5011.type = 'text/css';
+        link5011.rel = 'stylesheet';
+        link5011.href = '//res.cloudinary.com/spiralyze/raw/upload/v1702028462/ABCFitnessIgnite/5011/code/5011-style.css';
+        document.head.appendChild(link5011);
+
+        //5009 Winner test changes starts
+        let btnHtml
+        if (window.location.pathname.indexOf('/pt/') > -1) {
+            btnHtml = `<li class="spz-menu-item">
+                <a class="spz-demo-btn" href="#">Quero testar</a>
+            </li>`;
+        } else if (window.location.pathname.indexOf('/es/') > -1) {
+            btnHtml = `<li class="spz-menu-item">
+                <a class="spz-demo-btn" href="#">PruÃ©belo</a>
+            </li>`;
+        } else {
+            btnHtml = `<li class="spz-menu-item">
+                <a class="spz-demo-btn" href="#">GET A DEMO</a>
+            </li>`;
+        }
+
+        waitForElm('.site-header #mega-menu-primary').then((elm) => {
+            elm.insertAdjacentHTML("beforeend", btnHtml)
+            setTimeout(() => {
+                // if (window.location.pathname == '/') {
+                document.querySelector('.spz-demo-btn').addEventListener('click', function (e) {
+                    e.preventDefault();
+                    // document.querySelector('.modal-flydown-trigger').click()
+                    window.location.href = 'https://abcfitness.com/request-a-demo/'
+                    return false;
+                })
+                // }
+            }, 100)
+        });
+
+        if (window.location.pathname == '/' || window.location.pathname == '/pt/' || window.location.pathname == '/es/') {
+            waitForElm('.modal-flydown-trigger').then((elm) => {
+                if (window.location.pathname.indexOf('/pt/') > -1) {
+                    elm.innerText = 'SOLICITE UMA DEMO';
+                } else if (window.location.pathname.indexOf('/es/') > -1) {
+                    elm.innerText = 'SOLICITA UNA DEMO';
+                } else {
+                    // elm.innerText = 'REQUEST A DEMO';
+                }
+            });
+        }
+
+        window.addEventListener('scroll', function () {
+            var headerNav = document.querySelector(".bkg-header");
+            let getScrollposition = window.scrollY;
+            if (getScrollposition > headerNav.offsetTop) {
+                headerNav.classList.add('fixed-header')
+            } else {
+                if (headerNav.classList.contains('fixed-header')) {
+                    headerNav.classList.remove('fixed-header')
+                }
+            }
+        })
+
+    }
+
 }
 
 function waitForElm(selector) {
