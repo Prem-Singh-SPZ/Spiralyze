379e1c3b84586028528e12f69d96bc43ad920834
Prem-Singh-SPZ <prem@spiralyze.com>
2024-02-07 10:46:18 +0530

    main

--- Komodo/Speaker/script.js
+++ Komodo/Speaker/script.js
@@ -1,10 +1,10 @@
 document.addEventListener("DOMContentLoaded", (event) => {
     var modal = document.querySelector(".modal");
-    var trigger = document.querySelector(".speaker-card");
     var closeButton = document.querySelector(".close-button");
 
     function toggleModal() {
         modal.classList.toggle("show-modal");
+        document.body.classList.toggle("no-scroll");
     }
 
     function windowOnClick(event) {
@@ -14,13 +14,13 @@ document.addEventListener("DOMContentLoaded", (event) => {
     }
 
     window.addEventListener("click", function (e) {
-        if (e.target.classList.contains('speaker-bio')) {
+        if (e.target.classList.contains('card-img')) {
             console.log(e.target)
             toggleModal();
         }
     });
 
-    trigger.addEventListener("click", toggleModal);
+    // trigger.addEventListener("click", toggleModal);
     closeButton.addEventListener("click", toggleModal);
     window.addEventListener("click", windowOnClick);
 });
