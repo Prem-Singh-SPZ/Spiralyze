8a1d82d62daaa41f78d4b746bdcca4855d4257a9
Prem-Singh-SPZ <prem@spiralyze.com>
2024-04-08 09:38:45 +0530

    8005

--- Netskope/8005/8005-script.js
+++ Netskope/8005/8005-script.js
@@ -1,100 +1,100 @@
-let bodyLoaded = setInterval(function () {
-    const body = document.querySelector('body');
-    if (body) {
-        clearInterval(bodyLoaded);
+// let bodyLoaded = setInterval(function () {
+//     const body = document.querySelector('body');
+//     if (body) {
+//         clearInterval(bodyLoaded);
 
-        if (!body.classList.contains('spz-8005')) {
-            body.classList.add('spz-8005');
+if (!document.body.classList.contains('spz-8005')) {
+    document.body.classList.add('spz-8005');
 
-            waitForElm('.spz-8005 .gated-content__top .gated-content__banner').then(function () {
+    waitForElm('.spz-8005 .gated-content__top .gated-content__banner').then(function () {
 
-                //Form internal code
-                waitForElm('body form.mktoForm .mktoFormCol .mktoFieldWrap input').then(function () {
-                    formModify();
-                    setHiddenFields();
-                });
+        //Form internal code
+        waitForElm('body form.mktoForm .mktoFormCol .mktoFieldWrap input').then(function () {
+            formModify();
+            setHiddenFields();
+        });
 
-                /*Truecontrolclass*/
-                var BodyClasses = {
-                    "delivering-on-the-promise-of-100-legacy-vpn-retirement": "resources_whitepapers",
-                    "why-you-should-and-how-you-can-move-away-from-existing-dlp-programs": "resources_dlp",
-                    "ebooks/the-6-most-compelling-use-cases-for-complete-legacy-vpn-replacement": "spz-mos",
-                    "ebooks/security-service-edge-sse-for-dummies": "spz-cur",
-                    "ebooks/modern-data-loss-prevention-dlp-for-dummies": "spz-str"
+        /*Truecontrolclass*/
+        var BodyClasses = {
+            "delivering-on-the-promise-of-100-legacy-vpn-retirement": "resources_whitepapers",
+            "why-you-should-and-how-you-can-move-away-from-existing-dlp-programs": "resources_dlp",
+            "ebooks/the-6-most-compelling-use-cases-for-complete-legacy-vpn-replacement": "spz-mos",
+            "ebooks/security-service-edge-sse-for-dummies": "spz-cur",
+            "ebooks/modern-data-loss-prevention-dlp-for-dummies": "spz-str"
 
-                };
-                var currentUrl = window.location.href;
-                for (var key in BodyClasses) {
-                    if (currentUrl.includes(key)) {
-                        body.classList.add(BodyClasses[key]);
-                    }
-                }
-
-                // Right section content relocation
-                document.querySelector('.gated-content__container .gated-content__right').insertAdjacentElement('afterbegin', document.querySelector('.gated-content__top .breadcrumbs'));
-                document.querySelector('.gated-content__container .gated-content__right').insertAdjacentElement('afterbegin', document.querySelector('#wrapper .header.has-promo-banner'));
-                document.querySelector('.gated-content__container .gated-content__left').insertAdjacentElement('afterbegin', document.querySelector('.gated-content__top .gated-content__banner'));
-                if(document.querySelector('#components__content .related-resources-cards')){
-                    document.querySelector('.gated-content__container .gated-content__left').insertAdjacentElement('beforeend', document.querySelector('#components__content .related-resources-cards'));
-                }
-
-                //Footer addition
-
-                let facebookHref = document.querySelector('.v3-social-icons__icon--facebook a').getAttribute('href');
-                let twitterHref = document.querySelector('.v3-social-icons__icon--twitter a').getAttribute('href');
-                let linkedinHref = document.querySelector('.v3-social-icons__icon--linkedin a').getAttribute('href');
-                document.querySelector('.gated-content__container .gated-content__left').insertAdjacentHTML('beforeend', `<div class="spz-footer-section"><div class="footer-container"><div class="u-footer"><div class="nmobile"><img src="//res.cloudinary.com/spiralyze/image/upload/v1706077616/netskope/9001/Call-icon.svg" alt="phone"> USA: +1 (800) 979-6988</div><div class="social-icons"><div class="social-icon icon--facebook"> <a href="` + facebookHref + `"> <img src="//res.cloudinary.com/spiralyze/image/upload/v1708099742/netskope/8005/facebook_logo.svg" class="s-icon" alt="facebook icon"></a></div><div class="social-icon icon--twitter"> <a href="` + twitterHref + `"> <img src="//res.cloudinary.com/spiralyze/image/upload/v1708099742/netskope/8005/x_logo.svg" class="s-icon" alt="twitter icon"></a></div><div class="social-icon icon--linkedin"> <a href="` + linkedinHref + `"> <img src="//res.cloudinary.com/spiralyze/image/upload/v1708099742/netskope/8005/linkedin_logo.svg" class="s-icon" alt="twitter icon"></a></div></div></div><div class="l-footer"><ul class="navbar-nav"><li class="menu-item"><a href="https://www.netskope.com/privacy-policy">Privacy Policy</a></li><li class="menu-item"><a href="https://www.netskope.com/vulnerability-disclosure-policy">Vulnerability Disclosure Policy</a></li><li class="menu-item"><a href="https://www.netskope.com/terms-of-use">Terms of Use</a></li><li class="menu-item"><a href="https://www.netskope.com/netskope-technical-support">Support</a></li></ul><div class="v3-footer__date">© 2024, All rights reserved.</div></div></div></div>`);
-
-                //on window resize run the function
-                iconUpdate();
-                window.addEventListener('resize', function () {
-                    iconUpdate();
-                });
-
-                //header menu changes
-                document.querySelector('.header__mobile-nav .header__mobile-nav-list').insertAdjacentHTML('beforeend', `<li class="header__mobile-nav-item"><a href="/netskope-technical-support" class="header__mobile-nav-item-link">Support</a></li>`);
-            });
-
-            // on click of .header_mobile__navbar-toggler add class in parent
-            document.querySelector('.header_mobile__navbar-toggler').addEventListener('click', function () {
-                checkMenuIsOpen();
-            });
-
-            //on window resize run the function
-            window.addEventListener('resize', function () {
-                checkMenuIsOpen();
-            });
-
-            //click event listener for document
-            window.addEventListener('click', function (event) {
-                if (event.target.classList.contains('header__search-input')) {
-                    if (this.document.getElementById('spz-8005')) {
-                        document.getElementById('spz-8005').remove();
-                    }
-                    let width = document.querySelector('.header__top.has-promo-banner').offsetWidth - 64 + 'px';
-                    let left = document.querySelector('.header__top.has-promo-banner').getBoundingClientRect().left + 32 + 'px';
-                    let top = document.querySelector('.header__top.has-promo-banner').getBoundingClientRect().top + 80 + 'px';
-
-                    if ((window.innerWidth > 767) && (window.innerWidth < 1024)) {
-                        width = 600 + 'px';
-                        top = 73 + 'px';
-                        left = (this.window.innerWidth - 736) + 'px';
-                    }
-
-                    if ((window.innerWidth < 768)) {
-                        width = document.querySelector('.header__top.has-promo-banner').offsetWidth - 48 + 'px';
-                        top = document.querySelector('.header__top.has-promo-banner').getBoundingClientRect().top + 67 + 'px';
-                        left = document.querySelector('.header__top.has-promo-banner').getBoundingClientRect().left + 24 + 'px';
-                    }
-
-                    document.querySelector('.algolia-autocomplete').insertAdjacentHTML('afterend', `<style id="spz-8005">.spz-8005 .algolia-autocomplete{width: ${width}!important; left: ${left}!important; top: ${top}!important;}</style>`);
-                }
-            });
-
+        };
+        var currentUrl = window.location.href;
+        for (var key in BodyClasses) {
+            if (currentUrl.includes(key)) {
+                document.body.classList.add(BodyClasses[key]);
+            }
         }
 
-    }
-});
+        // Right section content relocation
+        document.querySelector('.gated-content__container .gated-content__right').insertAdjacentElement('afterbegin', document.querySelector('.gated-content__top .breadcrumbs'));
+        document.querySelector('.gated-content__container .gated-content__right').insertAdjacentElement('afterbegin', document.querySelector('#wrapper .header.has-promo-banner'));
+        document.querySelector('.gated-content__container .gated-content__left').insertAdjacentElement('afterbegin', document.querySelector('.gated-content__top .gated-content__banner'));
+        if (document.querySelector('#components__content .related-resources-cards')) {
+            document.querySelector('.gated-content__container .gated-content__left').insertAdjacentElement('beforeend', document.querySelector('#components__content .related-resources-cards'));
+        }
+
+        //Footer addition
+
+        let facebookHref = document.querySelector('.v3-social-icons__icon--facebook a').getAttribute('href');
+        let twitterHref = document.querySelector('.v3-social-icons__icon--twitter a').getAttribute('href');
+        let linkedinHref = document.querySelector('.v3-social-icons__icon--linkedin a').getAttribute('href');
+        document.querySelector('.gated-content__container .gated-content__left').insertAdjacentHTML('beforeend', `<div class="spz-footer-section"><div class="footer-container"><div class="u-footer"><div class="nmobile"><img src="//res.cloudinary.com/spiralyze/image/upload/v1706077616/netskope/9001/Call-icon.svg" alt="phone"> USA: +1 (800) 979-6988</div><div class="social-icons"><div class="social-icon icon--facebook"> <a href="` + facebookHref + `"> <img src="//res.cloudinary.com/spiralyze/image/upload/v1708099742/netskope/8005/facebook_logo.svg" class="s-icon" alt="facebook icon"></a></div><div class="social-icon icon--twitter"> <a href="` + twitterHref + `"> <img src="//res.cloudinary.com/spiralyze/image/upload/v1708099742/netskope/8005/x_logo.svg" class="s-icon" alt="twitter icon"></a></div><div class="social-icon icon--linkedin"> <a href="` + linkedinHref + `"> <img src="//res.cloudinary.com/spiralyze/image/upload/v1708099742/netskope/8005/linkedin_logo.svg" class="s-icon" alt="twitter icon"></a></div></div></div><div class="l-footer"><ul class="navbar-nav"><li class="menu-item"><a href="https://www.netskope.com/privacy-policy">Privacy Policy</a></li><li class="menu-item"><a href="https://www.netskope.com/vulnerability-disclosure-policy">Vulnerability Disclosure Policy</a></li><li class="menu-item"><a href="https://www.netskope.com/terms-of-use">Terms of Use</a></li><li class="menu-item"><a href="https://www.netskope.com/netskope-technical-support">Support</a></li></ul><div class="v3-footer__date">© 2024, All rights reserved.</div></div></div></div>`);
+
+        //on window resize run the function
+        iconUpdate();
+        window.addEventListener('resize', function () {
+            iconUpdate();
+        });
+
+        //header menu changes
+        document.querySelector('.header__mobile-nav .header__mobile-nav-list').insertAdjacentHTML('beforeend', `<li class="header__mobile-nav-item"><a href="/netskope-technical-support" class="header__mobile-nav-item-link">Support</a></li>`);
+    });
+
+    // on click of .header_mobile__navbar-toggler add class in parent
+    document.querySelector('.header_mobile__navbar-toggler').addEventListener('click', function () {
+        checkMenuIsOpen();
+    });
+
+    //on window resize run the function
+    window.addEventListener('resize', function () {
+        checkMenuIsOpen();
+    });
+
+    //click event listener for document
+    window.addEventListener('click', function (event) {
+        if (event.target.classList.contains('header__search-input')) {
+            if (this.document.getElementById('spz-8005')) {
+                document.getElementById('spz-8005').remove();
+            }
+            let width = document.querySelector('.header__top.has-promo-banner').offsetWidth - 64 + 'px';
+            let left = document.querySelector('.header__top.has-promo-banner').getBoundingClientRect().left + 32 + 'px';
+            let top = document.querySelector('.header__top.has-promo-banner').getBoundingClientRect().top + 80 + 'px';
+
+            if ((window.innerWidth > 767) && (window.innerWidth < 1024)) {
+                width = 600 + 'px';
+                top = 73 + 'px';
+                left = (this.window.innerWidth - 736) + 'px';
+            }
+
+            if ((window.innerWidth < 768)) {
+                width = document.querySelector('.header__top.has-promo-banner').offsetWidth - 48 + 'px';
+                top = document.querySelector('.header__top.has-promo-banner').getBoundingClientRect().top + 67 + 'px';
+                left = document.querySelector('.header__top.has-promo-banner').getBoundingClientRect().left + 24 + 'px';
+            }
+
+            document.querySelector('.algolia-autocomplete').insertAdjacentHTML('afterend', `<style id="spz-8005">.spz-8005 .algolia-autocomplete{width: ${width}!important; left: ${left}!important; top: ${top}!important;}</style>`);
+        }
+    });
+
+}
+
+//     }
+// });
 
 function checkMenuIsOpen() {
     if (document.querySelector('#header__mobile-nav-container[style*="display: block;"]')) {
@@ -112,7 +112,7 @@ function iconUpdate() {
         document.querySelector('.header__top-menu-link-language img').src = '//res.cloudinary.com/spiralyze/image/upload/v1711975619/netskope/8005/chevron-down-light.svg';
         document.querySelector('.header__top-menu-link--search-mobile img').src = '//res.cloudinary.com/spiralyze/image/upload/v1711975525/netskope/8005/search-light.svg';
         document.querySelector('.header__logo-image--white img').src = '//res.cloudinary.com/spiralyze/image/upload/v1711975525/netskope/8005/logo-light.svg';
-        if(document.querySelector('.related-resources-cards .filters__content .filters__wrapper')){
+        if (document.querySelector('.related-resources-cards .filters__content .filters__wrapper')) {
             document.querySelector('.related-resources-cards .filters__content .filters__wrapper').classList.add('filters__wrapper--list');
         }
         document.querySelector('.header__search-container-mobile .header__search-close-mobile').setAttribute('src', '//res.cloudinary.com/spiralyze/image/upload/v1712038573/netskope/8005/ross-circle-light.svg');
@@ -130,7 +130,7 @@ function iconUpdate() {
         document.querySelector('.header__top-menu-link-language img').src = '//res.cloudinary.com/spiralyze/image/upload/v1711354290/netskope/8005/chevron-down.svg';
         document.querySelector('.header__top-menu-link--search-mobile img').src = '//res.cloudinary.com/spiralyze/image/upload/v1711975524/netskope/8005/search-dark.svg';
         document.querySelector('.header__logo-image--white img').src = '//res.cloudinary.com/spiralyze/image/upload/v1711975524/netskope/8005/logo-dark.svg';
-        if(document.querySelector('.related-resources-cards .filters__content .filters__wrapper')){
+        if (document.querySelector('.related-resources-cards .filters__content .filters__wrapper')) {
             document.querySelector('.related-resources-cards .filters__content .filters__wrapper').classList.add('filters__wrapper--list');
         }
         document.querySelector('.header__search-container-mobile .header__search-close-mobile').setAttribute('src', '//res.cloudinary.com/spiralyze/image/upload/v1712038573/netskope/8005/ross-circle-dark.svg');
