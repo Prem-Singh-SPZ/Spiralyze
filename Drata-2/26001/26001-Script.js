d38de1199d16033f6c53f19e918ae1b7f7aaecb5
Prem-Singh-SPZ <prem@spiralyze.com>
2024-05-10 11:11:29 +0530

    26001

--- Drata-2/26001/26001-Script.js
+++ Drata-2/26001/26001-Script.js
@@ -1,8 +1,5 @@
-// COntrol URL
-// https://drata.com/blog/supply-chain-security?mutiny_preview=eyJhbGciOiJIUzI1NiJ9.eyJrZXkiOiI2NDAzZWJlMC1lMGIwLTRmYTUtYWViOS1mNzYwODM0ZTkyZWIiLCJpc3MiOiI4NDFhMTYzYjg1ZWFhM2YxIiwiY29tcGFueV9pZCI6Ijg0MWExNjNiODVlYWEzZjEiLCJleHBlcmllbmNlX2lkIjoiZDRiM2RhYzAtYjE0Mi00MDIzLTlhNTgtMWQ3ZGJiZWEzZGUwIiwidmFyaWF0aW9uX2lkIjoiNDMxOTJiZGItOGEyNy00NzU4LWFiZTEtZDZlMmY2MzUzNmY2IiwiZXhwIjoxNzAxMTg2Mzg5LCJ2YXJpYWJsZV9vdmVycmlkZXMiOnt9fQ.HcOeFi_9lPiEXCrZDQfNfgYRlZCOT6V89UHyqbcrkEs
-
 function createTest3002() {
-    document.body.classList.add('spz-3002-drataverse');
+    document.body.classList.add('spz-26001');
 
     waitForElm('#__next .hs-form-private').then(function () {
         insertStickySection();
@@ -19,10 +16,14 @@ function createTest3002() {
 //perform click actions
 window.addEventListener("click", function (e) {
     // console.log(e.target)
-    if (e.target.classList.contains("sticky-close-btn")) {
-        this.document.querySelector('.spz-sticky-footer').remove();
-        document.querySelector('#__next').style.paddingBottom = "0px";
-    }
+    // if (e.target.classList.contains("sticky-close-btn")) {
+    //     this.document.querySelector('.spz-sticky-footer').remove();
+    //     document.querySelector('#__next').style.paddingBottom = "0px";
+    // }
+    // console.log(e.target.classList)
+    // if (e.target.classList.contains("trigger-demo-btn")) {
+    //     document.querySelector('a[href="/demo"]').click();
+    // }
 });
 
 
@@ -31,15 +32,21 @@ function insertStickySection() {
         document.querySelector('#__next').insertAdjacentHTML('beforeend', `<div class="spz-sticky-footer"><div class="sticky-footer">
         <div class="sticky-container">
             <div class="left-content">
-                <img class="left-arrow" src="https://res.cloudinary.com/spiralyze/image/upload/v1690987004/drata/3002/decoration-arrow.svg" alt="Arrow" />
-                <span>Witness our biggest product updates yet at Drataverse Digital</span>
+                <span>Webinar: Grow Revenue with Compliance</span>
+            </div>
+            <div class="center-content">
+                <img class="g2-logo" src="https://res.cloudinary.com/spiralyze/image/upload/v1690986738/drata/3002/hero_logo-g2.svg" alt="G2 Logo" />
+                <img class="star-ratings" src="https://res.cloudinary.com/spiralyze/image/upload/v1690986738/drata/3002/rating-4_9.svg" alt="Ratings" />
+                <div class="ratings"><span>4.9 </span> (700+ reviews)</div>
             </div>
             <div class="right-content">
-                <a href="https://drata.com/events/drataverse-oct-2023" target="_blank" class="hs-cta-primary trigger-demo-btn">Register Now <img src="https://res.cloudinary.com/spiralyze/image/upload/v1690987050/drata/3002/cta_arrow.svg" alt="Arrow" /></a>
+                <a href="/demo" target="_blank" class="hs-cta-primary trigger-demo-btn">Register Here <svg xmlns="http://www.w3.org/2000/svg" width="7" height="10" viewBox="0 0 7 10" fill="none">
+                <path d="M1 1L5 5L1 9" stroke="white" stroke-width="1.5" stroke-linecap="round"/>
+              </svg></a>
             </div>
             </div>
             <div class="close-sticky">
-            <a href="javascript:void(0)" class="sticky-close-btn"><img src="https://res.cloudinary.com/spiralyze/image/upload/v1690986863/drata/3002/close.svg" alt="Close" /></a>
+            <a href="https://drata.com/resources/webinars/accelerate-revenue-with-soc-2" class="sticky-close-btn"><img src="https://res.cloudinary.com/spiralyze/image/upload/v1690986863/drata/3002/close.svg" alt="Close" /></a>
             </div>
         </div>
     </div>
@@ -76,7 +83,7 @@ function removeTest() {
         document.querySelector('.spz-sticky-footer').remove();
         document.querySelector('#__next').style.paddingBottom = "0px";
     }
-    document.body.classList.remove("spz-3002-drataverse");
+    document.body.classList.remove("spz-26001");
 }
 
 history.pushState = (function (f) {
@@ -109,11 +116,7 @@ urlCheck(url);
 
 function urlCheck(url) {
     let testURL = '';
-    if (window.location.href.indexOf("/product") > -1 ||
-        window.location.href.indexOf("/platform") > -1 ||
-        window.location.href.indexOf("/blog") > -1 ||
-        window.location.href.indexOf("/resources/reports") > -1 ||
-        window.location.href == "https://drata.com/") {
+    if (window.location.href.indexOf("/platform/startup") > -1) {
         testURL = window.location.href;
     }
 
