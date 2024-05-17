b40c798f8acd0c63cd3c0cc4efabfe7e7ad41d92
Prem-Singh-SPZ <prem@spiralyze.com>
2024-05-17 12:59:32 +0530

    4014

--- Drata-2/4014/4014-script.js
+++ Drata-2/4014/4014-script.js
@@ -1,60 +1,23 @@
-//create json object to change hero copy
-const heroCopy = {
-    "gdpr": {
-        "heroTitle": `<span>Simplify</span> and <span>Fast-Track</span> GDPR Compliance`,
-        "herolist1": `Cut compliance work by 80%.<span class="ls-desc"> Drata collects evidence for you via integrations - no spreadsheets or screenshots required.</span>`,
-        "herolist2": `Ace your audits.<span class="ls-desc"> Get complete audit confidence with full visibility into your compliance status and auditor-preferred files.</span>`,
-        "herolist3": `Unblock sales with unmatched security.<span class="ls-desc"> Quickly publish and share compliance documents to speed through security reviews.</span>`,
-    },
-    "hipaa": {
-        "heroTitle": `<span>Simplify</span> and <span>Fast-Track</span> HIPAA Compliance`,
-        "herolist1": `Cut compliance work by 80%.<span class="ls-desc"> Drata collects evidence for you via integrations - no spreadsheets or screenshots required.</span>`,
-        "herolist2": `Ace your audits.<span class="ls-desc"> Get complete audit confidence with full visibility into your compliance status and auditor-preferred files.</span>`,
-        "herolist3": `Unblock sales with unmatched security.<span class="ls-desc"> Quickly publish and share compliance documents to speed through security reviews.</span>`,
-    },
-    "soc2": {
-        "heroTitle": `<span>Simplify</span> and <span>Fast-Track</span> SOC 2 Compliance`,
-        "herolist1": `Cut compliance work by 80%.<span class="ls-desc"> Drata collects evidence for you via integrations - no spreadsheets or screenshots required.</span>`,
-        "herolist2": `Ace your audits.<span class="ls-desc"> Get complete audit confidence with full visibility into your compliance status and auditor-preferred files.</span>`,
-        "herolist3": `Unblock sales with unmatched security.<span class="ls-desc"> Quickly publish and share compliance documents to speed through security reviews.</span>`,
-    },
-    "iso27001": {
-        "heroTitle": `<span>Simplify</span> and <span>Fast-Track</span> ISO 27001 Compliance`,
-        "herolist1": `Cut compliance work by 80%.<span class="ls-desc"> Drata collects evidence for you via integrations - no spreadsheets or screenshots required.</span>`,
-        "herolist2": `Ace your audits.<span class="ls-desc"> Get complete audit confidence with full visibility into your compliance status and auditor-preferred files.</span>`,
-        "herolist3": `Unblock sales with unmatched security.<span class="ls-desc"> Quickly publish and share compliance documents to speed through security reviews.</span>`,
-    }
-}
-
-//create a style tag and append in head 
-const style = document.createElement('style');
-style.innerHTML = `@media (min-width: 767.98px){.spz-4014 .hero-section .hero-content .hc-title{font-size: 52px;
-    font-weight: 700;
-    line-height: 64px;
-    letter-spacing: -1.04px;}}`;
-document.head.appendChild(style);
-
-
 // Set test class
 document.body.classList.add("spz-4014");
 
-waitForElm('.hero-content h1.hc-title').then(function () {
+waitForElm('.hero-section .list-grp-wrapper .list-group .list-item .ls-desc').then(function () {
     loadTest()
 });
 
 function loadTest() {
-    const heroTitle = document.querySelector('.hero-content h1.hc-title');
-    const heroList1 = document.querySelector('.list-grp-wrapper .list-group li:nth-child(1) .ls-title');
-    const heroList2 = document.querySelector('.list-grp-wrapper .list-group li:nth-child(2) .ls-title');
-    const heroList3 = document.querySelector('.list-grp-wrapper .list-group li:nth-child(3) .ls-title');
+    document.querySelector('.hero-section .list-grp-wrapper .list-group .list-item:nth-child(2) .ls-desc').textContent = 'Automate compliance for 18+ frameworks including SOC 2, ISO 27001, GDPR, Cyber Essentials and more.';
+    document.querySelector('.hero-section .list-grp-wrapper .list-group .list-item:nth-child(3) .ls-desc').textContent = '20+ customisable, auditor approved policies. Streamlined employee signing & documentation.';
 
-    heroTitle.innerHTML = heroCopy[page].heroTitle;
-    heroList1.innerHTML = heroCopy[page].herolist1;
-    heroList2.innerHTML = heroCopy[page].herolist2;
-    heroList3.innerHTML = heroCopy[page].herolist3;
+    document.querySelector('.features-section .work-step-item:first-child .step-info').textContent = `Automated compliance for 18+ products and frameworks including SOC 2, ISO 27001, Cyber Essentials, GDPR, HIPAA, & PCI.`;
+    document.querySelector('.features-section .work-step-item:nth-child(3) .step-info').textContent = `20+ customisable, auditor-approved policies. Streamline employee signing & documentation.`;
+
+    waitForElm('.form-wrapper-spz .hbspt-form form fieldset .field.hs-fieldtype-checkbox>.input ul li>label>span').then(function () {
+        document.querySelector('.form-wrapper-spz .hbspt-form form fieldset .field.hs-fieldtype-checkbox>.input ul li:nth-child(3)>label>span').textContent = 'Cyber Essentials';
+        document.querySelector('.form-wrapper-spz form .form-columns-0 .hs-richtext.hs-main-font-element').textContent = 'For partnership inquiries, please contact partnerships@drata.com'
+    });
 
     // setHiddenFields();
-
 }
 
 function setHiddenFields() {
