482cc9898ac715f5cea2431907792ab505ca4d95
Prem-Singh-SPZ <prem@spiralyze.com>
2024-05-29 11:12:15 +0530

    2002

--- Zywave/2002/2002-script.js
+++ Zywave/2002/2002-script.js
@@ -11,8 +11,8 @@ function pageModify() {
     if (document.querySelectorAll('.zy-homepage-hero-container .zy-homepage-hero-container-inner .spz-hero-section').length == 0) {
         document.querySelector('.zy-homepage-hero-container .zy-homepage-hero-container-inner').insertAdjacentHTML('beforeend', `<div class="spz-hero-section">
         <div class="hero-title-wrapper">
-         <h1 class="h-title">Automate insurance sales <br>and retention. Boost revenue. </h1>
-         <p class="h-subtitle">Find qualified prospects. Send instant quotes. Let clients manage plans from an online portal. Generate benefit booklets, compliance guides and more.</p>
+         <h1 class="h-title">Streamline operations and <br>maximize revenue.</h1>
+         <p class="h-subtitle">Improve efficiency of prospecting and selling. Make smarter decisions with data-driven insights. Increase productivity and achieve profitable growth. </p>
         </div>
         <div class="hero-email-cta">
          <form class="form-spz" id="">
