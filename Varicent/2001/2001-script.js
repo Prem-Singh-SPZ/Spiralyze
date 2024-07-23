186c335427fa0fe3b6de6e5d0de3fd5c5ec9d5ef
Prem-Singh-SPZ <prem@spiralyze.com>
2024-07-23 09:16:17 +0530

    2001

--- Varicent/2001/2001-script.js
+++ Varicent/2001/2001-script.js
@@ -1,28 +1,3 @@
-/* CUSTOM CODE */
-window.addEventListener("message", function (event) {
-    if (event.data.type === 'hsFormCallback') {
-        if (Object.keys(event.data.data).length === 0 && event.data.data.constructor === Object) return;
-
-        var hubspotFormName = event.data.data[0].value;
-        window.VWO = window.VWO || [];
-        VWO.event = VWO.event || function () { VWO.push(["event"].concat([].slice.call(arguments))) };
-        if (event.data.eventName === "onFormSubmitted") {
-            VWO.event("spz_hubspot_form_submit1", {
-                "form_id": event.data.id,
-                "form_name": hubspotFormName
-            });
-
-
-        } else if (event.data.eventName === "onBeforeFormSubmit") {
-            VWO.event("spz_hubspot_form_before_submit1", {
-                "form_id": event.data.id,
-                "form_name": hubspotFormName
-            });
-
-        }
-    }
-});
-
 document.body.classList.add('spz-2001');
 
 waitForElm(`form.hs-form-private .hs_firstname input`).then(function (elm) {
@@ -32,9 +7,11 @@ waitForElm(`form.hs-form-private .hs_firstname input`).then(function (elm) {
 
 function hero_modify() {
 
-    document.querySelector('h1.h2').insertAdjacentHTML('afterend', '<div class="hero-copy d-md-block d-none"><p>Traditional sales and go-to-market planning efforts are failing. To succeed in today&#8217;s markets, you must be able to embrace change and adopt the new drivers of sales team success.</p><p><span class="fw-semibold">Ready to learn how?</span> This research shares insights on the current state of how sales and revenue leaders can set their teams up for success in today&#8217;s change intensive environment.</p></div>');
-    document.querySelector('.hs_cos_wrapper.hs_cos_wrapper_widget.hs_cos_wrapper_type_form').insertAdjacentHTML('afterend', '<div class="hero-copy d-md-none"><p>Traditional sales and go-to-market planning efforts are failing. To succeed in today&#8217;s markets, you must be able to embrace change and adopt the new drivers of sales team success.</p><p><span class="fw-semibold">Ready to learn how?</span> This research shares insights on the current state of how sales and revenue leaders can set their teams up for success in today&#8217;s change intensive environment.</p></div>');
-    // document.querySelector('h1.h2').outerHTML = '<h1 class="fw-bold my-4 spz-headline"><span class="w-text">Is your <span class="d-xl-block d-inline y-text">Sales Go-To-Market</span> Plan failing?</span></h1>';
+    if (document.querySelectorAll('.hero-copy.d-md-block').length == 0) {
+        document.querySelector('h1.h2').insertAdjacentHTML('afterend', '<div class="hero-copy d-md-block d-none"><p>Traditional sales and go-to-market planning efforts are failing. To succeed in today&#8217;s markets, you must be able to embrace change and adopt the new drivers of sales team success.</p><p><span class="fw-semibold">Ready to learn how?</span> This research shares insights on the current state of how sales and revenue leaders can set their teams up for success in today&#8217;s change intensive environment.</p></div>');
+        document.querySelector('.hs_cos_wrapper.hs_cos_wrapper_widget.hs_cos_wrapper_type_form').insertAdjacentHTML('afterend', '<div class="hero-copy d-md-none"><p>Traditional sales and go-to-market planning efforts are failing. To succeed in today&#8217;s markets, you must be able to embrace change and adopt the new drivers of sales team success.</p><p><span class="fw-semibold">Ready to learn how?</span> This research shares insights on the current state of how sales and revenue leaders can set their teams up for success in today&#8217;s change intensive environment.</p></div>');
+        // document.querySelector('h1.h2').outerHTML = '<h1 class="fw-bold my-4 spz-headline"><span class="w-text">Is your <span class="d-xl-block d-inline y-text">Sales Go-To-Market</span> Plan failing?</span></h1>';
+    }
 
     if (!sessionStorage.getItem('unblock')) {
         document.querySelector('.dnd_area-row-0-background-color.dnd-section.dnd_area-row-0-padding').insertAdjacentHTML('beforeend', '<a id="unblock_btn">LEARN MORE</a>');
