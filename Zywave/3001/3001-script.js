7bc2c6ef817ffe439053f4a92ecd108c0e881fb4
Prem-Singh-SPZ <prem@spiralyze.com>
2024-07-31 10:55:22 +0530

    3001

--- Zywave/3001/3001-script.js
+++ Zywave/3001/3001-script.js
@@ -83,47 +83,75 @@ function setTabsContent() {
                         <div id="tab1" class="tab-contents active">
                             <div class="accordion-wrapper ac-wrapper-1">
                                 <div class="left-copy">
-                                    <div class="title">Carrier pricing</div>
-                                    <div class="description">Access exclusive, highly-curated data sources — including lab, genomics, oncology, EMR data, race and ethnicity, social determinants of health, mortality, and more.</div>
+                                    <div class="title">Sales Cloud</div>
+                                    <div class="description">Streamline prospecting, follow-up, and renewal workflows. Find qualified leads. Connect directly in-app and follow up instantly. Auto-generate quotes.</div>
                                     <div class="cta-wrapper">
-                                        <a href="javascript:void(0);" class="spz-cta">get a demo</a>
+                                        <a href="javascript:void(0);" class="spz-cta">Get a Demo</a>
                                     </div>
                                 </div>
                                 <div class="right-img">
                                     <picture>
-                                        <source media="(max-width:767.98px)" srcset="//res.cloudinary.com/spiralyze/image/upload/f_auto/komodohealth/3008/specialty_data_enhancements_1440.webp">
-                                        <img src="//res.cloudinary.com/spiralyze/image/upload/f_auto/komodohealth/3008/specialty_data_enhancements_1440.webp" alt="Specialty Data Enhancements" width="688" height="404">
+                                        <source media="(min-width:1024.98px)" srcset="//res.cloudinary.com/spiralyze/image/upload/f_auto/zywave/3001/sales_cloud_1.webp">
+                                        <source media="(min-width:767.98px)" srcset="//res.cloudinary.com/spiralyze/image/upload/f_auto/zywave/3001/sales_cloud__tablet.webp">
+                                        <img src="//res.cloudinary.com/spiralyze/image/upload/f_auto/zywave/3001/sales_cloud__mobile.webp" alt="Sales Cloud">
                                     </picture>
                                 </div>
                             </div>
                         </div>
                         <div id="tab2" class="tab-contents">
                             <div class="accordion-wrapper ac-wrapper-2">
-                                <div class="child">
-                                    <div class="bar"><div class="line"></div></div>
-                                    <div class="title">
-                                    <div class="text">Carrier pricing</div>
+                                <div class="left-copy">
+                                    <div class="title">Client Cloud</div>
+                                    <div class="description">Streamline prospecting, follow-up, and renewal workflows. Find qualified leads. Connect directly in-app and follow up instantly. Auto-generate quotes.</div>
+                                    <div class="cta-wrapper">
+                                        <a href="javascript:void(0);" class="spz-cta">Get a Demo</a>
                                     </div>
-                                    <div class="content">
-                                    <div class="text">Access exclusive, highly-curated data sources — including lab, genomics, oncology, EMR data, race and ethnicity, social determinants of health, mortality, and more.</div>
+                                </div>
+                                <div class="right-img">
                                     <picture>
-                                        <source media="(max-width:767.98px)" srcset="//res.cloudinary.com/spiralyze/image/upload/f_auto/komodohealth/3008/specialty_data_enhancements_1440.webp">
-                                        <img src="//res.cloudinary.com/spiralyze/image/upload/f_auto/komodohealth/3008/specialty_data_enhancements_1440.webp" alt="Specialty Data Enhancements" width="688" height="404">
+                                        <source media="(min-width:1024.98px)" srcset="//res.cloudinary.com/spiralyze/image/upload/f_auto/zywave/3001/client_cloud_1.webp">
+                                        <source media="(min-width:767.98px)" srcset="//res.cloudinary.com/spiralyze/image/upload/f_auto/zywave/3001/client_cloud_tablet.webp">
+                                        <img src="//res.cloudinary.com/spiralyze/image/upload/f_auto/zywave/3001/client_cloud_mobile.webp" alt="Client Cloud">
                                     </picture>
-                                    <a href="#popup_request_a_demo" rel="modal:open" class="cta">
-                                        <span>get a demo</span>
-                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16" fill="none">
-                                        <path d="M6 12L10 8L6 4" stroke="currentColor" stroke-width="1.5" stroke-linecap="square"></path>
-                                        </svg>
-                                    </a>
-                                    </div>
                                 </div>
                             </div>
                         </div>
                        
                         <div id="tab3" class="tab-contents">
+                            <div class="accordion-wrapper ac-wrapper-3">
+                                <div class="left-copy">
+                                    <div class="title">Content Cloud</div>
+                                    <div class="description">Streamline prospecting, follow-up, and renewal workflows. Find qualified leads. Connect directly in-app and follow up instantly. Auto-generate quotes.</div>
+                                    <div class="cta-wrapper">
+                                        <a href="javascript:void(0);" class="spz-cta">Get a Demo</a>
+                                    </div>
+                                </div>
+                                <div class="right-img">
+                                    <picture>
+                                        <source media="(min-width:1024.98px)" srcset="//res.cloudinary.com/spiralyze/image/upload/f_auto/zywave/3001/content_cloud_1.webp">
+                                        <source media="(min-width:767.98px)" srcset="//res.cloudinary.com/spiralyze/image/upload/f_auto/zywave/3001/content_cloud_tablet.webp">
+                                        <img src="//res.cloudinary.com/spiralyze/image/upload/f_auto/zywave/3001/content_cloud_mobile.webp" alt="Content Cloud">
+                                    </picture>
+                                </div>
+                            </div>
                         </div>
                          <div id="tab4" class="tab-contents">
+                            <div class="accordion-wrapper ac-wrapper-4">
+                                <div class="left-copy">
+                                    <div class="title">Analytics Cloud</div>
+                                    <div class="description">Streamline prospecting, follow-up, and renewal workflows. Find qualified leads. Connect directly in-app and follow up instantly. Auto-generate quotes.</div>
+                                    <div class="cta-wrapper">
+                                        <a href="javascript:void(0);" class="spz-cta">Get a Demo</a>
+                                    </div>
+                                </div>
+                                <div class="right-img">
+                                    <picture>
+                                        <source media="(min-width:1024.98px)" srcset="//res.cloudinary.com/spiralyze/image/upload/f_auto/zywave/3001/analytics_cloud_1.webp">
+                                        <source media="(min-width:767.98px)" srcset="//res.cloudinary.com/spiralyze/image/upload/f_auto/zywave/3001/analytics_cloud_tablet.webp">
+                                        <img src="//res.cloudinary.com/spiralyze/image/upload/f_auto/zywave/3001/analytics_cloud_mobile.webp" alt="Analytics Cloud">
+                                    </picture>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -185,18 +213,9 @@ function footer() {
     document.querySelector('.accordion-section').insertAdjacentHTML('afterend', `<div class="footer-section">
         <div class="footer-row">
             <div class="footer-col">
-                    <div class="footer-text">Zywave, Inc.</div>
-                    <div class="footer-social">
-                        <div class="social-link">
-                            <a href="https://linkedin.com/zywave" target="_blank"><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="none">
-                            <path d="M5.78335 4.16652C5.78313 4.60855 5.60732 5.03239 5.2946 5.34479C4.98188 5.6572 4.55787 5.83258 4.11585 5.83236C3.67382 5.83214 3.24998 5.65633 2.93758 5.34361C2.62517 5.0309 2.44979 4.60688 2.45001 4.16486C2.45023 3.72283 2.62604 3.29899 2.93876 2.98659C3.25147 2.67419 3.67548 2.4988 4.11751 2.49902C4.55954 2.49924 4.98338 2.67505 5.29578 2.98777C5.60818 3.30049 5.78357 3.7245 5.78335 4.16652ZM5.83335 7.06652H2.50001V17.4999H5.83335V7.06652ZM11.1 7.06652H7.78335V17.4999H11.0667V12.0249C11.0667 8.97486 15.0417 8.69152 15.0417 12.0249V17.4999H18.3333V10.8915C18.3333 5.74986 12.45 5.94152 11.0667 8.46652L11.1 7.06652Z" fill="white"/>
-                            </svg></a>
-                        </div>
-                        <div class="social-link">
-                            <a href="https://www.facebook.com/Zywave/" target="_blank"><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="none">
-                            <path d="M7.665 17.9168H10.9983V11.2418H14.0017L14.3317 7.92516H10.9983V6.25016C10.9983 6.02915 11.0861 5.81719 11.2424 5.66091C11.3987 5.50463 11.6107 5.41683 11.8317 5.41683H14.3317V2.0835H11.8317C10.7266 2.0835 9.66679 2.52248 8.88539 3.30388C8.10399 4.08529 7.665 5.14509 7.665 6.25016V7.92516H5.99833L5.66833 11.2418H7.665V17.9168Z" fill="white"/>
-                            </svg></a>
-                        </div>
+                    <div class="footer-text">© 2024 ZYWAVE</div>
+                    <div class="footer-links">
+                        <ul class="spz-subfooter-menu"><li><a href="/terms-conditions/">Terms &amp; Conditions</a></li><li><a href="/privacy-statement/">Privacy</a></li><li><a href="/training-cancellation-policy/">Training Cancellation Policy</a></li><li><a href="/dmca-notice/">DMCA Notice</a></li><li><a href="/hipaa-security/">HIPAA Security</a></li></ul>
                     </div>
                 </div>
             </div>
