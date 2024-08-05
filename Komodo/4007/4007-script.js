da5c9cb0deb38893939787c680e16b09e4908c73
Prem-Singh-SPZ <prem@spiralyze.com>
2024-08-05 15:19:17 +0530

    4007

--- Komodo/4007/4007-script.js
+++ Komodo/4007/4007-script.js
@@ -10,8 +10,10 @@ function loadTest() {
     // Set test class
     document.body.classList.add('spz-4007');
 
-    if (document.querySelector('.spz-4007 .komodo-header #topnav-contact')) {
-        document.querySelector('.spz-4007 .komodo-header #topnav-contact').textContent = 'Get a Demo';
-        document.querySelector('.spz-4007 .komodo-header #topnav-contact').setAttribute('href', '#popup_request_a_demo');
+    if (document.querySelector('.spz-4007 #topnav-contact')) {
+        document.querySelectorAll('.spz-4007 #topnav-contact').forEach((el) => {
+            el.textContent = 'Get a Demo';
+            el.setAttribute('href', '#popup_request_a_demo');
+        });
     }
 }
