e7666e7189bf9dddcfffa046a0d75ee4a38ad939
Prem-Singh-SPZ <prem@spiralyze.com>
2024-09-03 11:06:10 +0530

    2004

---
+++ Varicent/2004/2004-script.js
@@ -0,0 +1,35 @@
+
+let bodyLoaded = setInterval(function () {
+	const body = document.querySelector('body');
+	if (body) {
+		clearInterval(bodyLoaded);
+		if (!body.classList.contains('spz-2004')) {
+			body.classList.add('spz-2004');
+
+            waitForElm('.spz-2004 .row-number-1 .widget-type-rich_text .hs_cos_wrapper.hs_cos_wrapper_widget').then(function () {
+
+			});
+
+			waitForElm('.spz-2004 .widget-type-form form.hs-form.hs-custom-form .hs-form-field input').then(function () {
+
+			});
+		}
+	}
+});
+
+
+// Generic Code
+function waitForElm(selector) {
+	return new Promise(function (resolve) {
+		if (document.querySelector(selector)) {
+			return resolve(document.querySelector(selector));
+		}
+		const observer = new MutationObserver(function (mutations) {
+			if (document.querySelector(selector)) {
+				resolve(document.querySelector(selector));
+				observer.disconnect();
+			}
+		});
+		observer.observe(document, { attributes: true, childList: true, subtree: true, characterData: true });
+	});
+}
