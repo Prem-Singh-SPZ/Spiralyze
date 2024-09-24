0cf7a79dae3368918ef9e9aed7ed5602f8fc8f01
Prem-Singh-SPZ <prem@spiralyze.com>
2024-09-24 09:18:00 +0530

    1006 Test fix

--- Komodo/1006 Update/1006-script.js
+++ Komodo/1006 Update/1006-script.js
@@ -13,7 +13,7 @@ ready(function () {
       if (document.querySelector('#popup_connect.wcs.wcs-modal') && document.querySelectorAll('.spz-left-contact').length == 0) {
         document.querySelector('#popup_connect.wcs.wcs-modal').remove();
         document.querySelector('#popup_contact_us .wcs-col-12').innerHTML = `
-        <div class="home-logo"><a href="https://www.komodohealth.com/"> <img src="//res.cloudinary.com/spiralyze/image/upload/v1707409447/komodohealth/1006/logo.webp" alt="Komodo Logo"></a></div>
+        <div class="home-logo"><a href="https://www.komodohealth.com/"> <img src="//res.cloudinary.com/spiralyze/image/upload/f_auto/komodohealth/1006/logo.webp" alt="Komodo Logo"></a></div>
         <div class="wcs-pform">
           <div class="wcs-popup-heading">
             <h2>Contact Us</h2>
@@ -22,18 +22,21 @@ ready(function () {
           </div>
         </div>
         <div class="social-proof"><picture>
-          <source media="(min-width:1025px)" srcset="https://res.cloudinary.com/spiralyze/image/upload/v1721285891/KomodoHealth/1006/Social_proof.png">
-          <source media="(min-width:768px)" srcset="https://res.cloudinary.com/spiralyze/image/upload/v1721285891/KomodoHealth/1006/Social_proof_tab.png">
-          <img src="https://res.cloudinary.com/spiralyze/image/upload/v1721285891/KomodoHealth/1006/Social_proof_mob.png" alt="Social Proof">
+          <source media="(min-width:1025px)" srcset="//res.cloudinary.com/spiralyze/image/upload/f_auto/KomodoHealth/1006/Social_proof.png">
+          <source media="(min-width:768px)" srcset="//res.cloudinary.com/spiralyze/image/upload/f_auto/KomodoHealth/1006/Social_proof_tab.png">
+          <img src="//res.cloudinary.com/spiralyze/image/upload/f_auto/KomodoHealth/1006/Social_proof_mob.png" alt="Social Proof">
         </picture></div>
         `
 
         document.querySelector('#popup_contact_us .wcs-col-12').insertAdjacentHTML("afterend", `<div class="wcs-bg-img"></div>`);
-        document.querySelector('#contact-us-form-wrapper').insertAdjacentElement("beforeend", document.querySelector('.widget-type-form div[data-hs-forms-root="true"]'));
-        // document.querySelector('body #new-form-wrapper').classList.add("step2");
-        document.body.insertAdjacentHTML("beforeend", `<div class="jquery-modal blocker current spz-left-contact"></div>`);
-        document.querySelector('.spz-1006 #popup_contact_us').classList.add('modal');
-        document.querySelector('.spz-left-contact').insertAdjacentElement("beforeend", document.querySelector('#popup_contact_us'));
+
+        waitForElm('body .widget-type-form div[data-hs-forms-root="true"]').then(() => {
+          document.querySelector('#contact-us-form-wrapper').insertAdjacentElement("beforeend", document.querySelector('.widget-type-form div[data-hs-forms-root="true"]'));
+          // document.querySelector('body #new-form-wrapper').classList.add("step2");
+          document.body.insertAdjacentHTML("beforeend", `<div class="jquery-modal blocker current spz-left-contact"></div>`);
+          document.querySelector('.spz-1006 #popup_contact_us').classList.add('modal');
+          document.querySelector('.spz-left-contact').insertAdjacentElement("beforeend", document.querySelector('#popup_contact_us'));
+        });
 
         let i = 0;
         const CROInterval = setInterval(() => {
