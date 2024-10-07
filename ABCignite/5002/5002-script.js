48076bde06d23e6767e1d366d8343320cfb06f63
Prem-Singh-SPZ <prem@spiralyze.com>
2024-10-07 12:23:43 +0530

    5002 init

--- ABCignite/5002/5002-script.js
+++ ABCignite/5002/5002-script.js
@@ -12,11 +12,13 @@ let bodyLoad = setInterval(function () {
         function loadTest() {
             document.querySelector('#demo-modal').classList.add('modal-container-spz');
             document.querySelector('#demo-modal .modal__container .demo-modal__col2 .demo-modal__form').insertAdjacentHTML('afterbegin', `<div class="logo-spz-bg">
-                <img src="//res.cloudinary.com/spiralyze/image/upload/v1706090193/ABCFitnessIgnite/5002/ignite-logo-bg.svg" class="ls-img">
+                <img src="//res.cloudinary.com/spiralyze/image/upload/v1728282479/ABCFitnessIgnite/5002/ABC_Logo.svg" class="ls-img">
             </div>`);
 
             // Add class 'spz-email-label' to parent .mktoFormCol of email label
             document.querySelector('#demo-modal #LblEmail').closest('.mktoFormCol').classList.add('spz-email-label');
+
+            focusFields();
         }
 
         function waitForElm(selector) {
@@ -38,5 +40,70 @@ let bodyLoad = setInterval(function () {
         if (navigator.userAgent.toLowerCase().indexOf('chrome/') == -1 && navigator.userAgent.toLowerCase().indexOf('safari/') > -1) {
             document.body.classList.add('safari')
         }
+
+
+        // On input focus add class on closest parent field class
+        function focusFields() {
+            document.querySelectorAll('#demo-modal .modal__container .mktoHasWidth.mktoField:not([type="checkbox"]):not([type="hidden"])').forEach(function (el) {
+                el.addEventListener('focus', function () {
+                    el.closest('.mktoFormCol').classList.add('field-focus');
+                });
+                el.addEventListener('blur', function () {
+                    el.closest('.mktoFormCol').classList.remove('field-focus');
+                    checkError(el);
+                });
+
+                // add event listeners to the input element
+                el.addEventListener('keypress', () => {
+                    checkError(el);
+                });
+
+                el.addEventListener('change', () => {
+                    checkError(el);
+                });
+
+                el.addEventListener('keydown', () => {
+                    checkError(el);
+                });
+
+                el.addEventListener('keyup', () => {
+                    checkError(el);
+                });
+            });
+        }
+
+        // Function to add .field-error class on closest parent .field class if .error is exist on input
+        function checkError(elem) {
+            document.querySelectorAll('#demo-modal .modal__container .mktoHasWidth.mktoField:not([type="checkbox"]):not([type="hidden"])').forEach(function (elem) {
+                let timeBuffer = setInterval(() => {
+                    if (elem.closest('.mktoFieldWrap').querySelector('.mktoError') && elem.closest('.mktoFieldWrap').querySelector('.mktoError').style.display != 'none' && elem.closest('.mktoFieldWrap').querySelector('.mktoInvalid')) {
+                        elem.closest('.mktoFormCol').classList.add('field-error');
+                    } else {
+                        elem.closest('.mktoFormCol').classList.remove('field-error');
+                    }
+                    if (elem && elem.value && (elem.value != '')) {
+                        // console.log(elem.value)
+                        elem.closest('.mktoFormCol').classList.add('input-filled');
+                        // elem.closest('.mktoFormCol').classList.remove('field-error');
+                    } else {
+                        elem.closest('.mktoFormCol').classList.remove('input-filled');
+                        // elem.closest('.mktoFormCol').classList.add('field-error');
+                    }
+
+                }, 100);
+
+                setTimeout(() => {
+                    clearInterval(timeBuffer);
+                }, 1000);
+            });
+
+
+        }
+
+        window.addEventListener("click", function (e) {
+            if (e.target.classList.contains("mktoButton")) {
+                checkError();
+            }
+        });
     }
 });
