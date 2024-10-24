683d7d26d8e044fe323306f04ddd9d91aab1675f
Prem-Singh-SPZ <prem@spiralyze.com>
2024-10-24 20:07:37 +0530

    hidden field logic

---
+++ Drata-2/Dropdown/TC-hidden-field.js
@@ -0,0 +1,145 @@
+(function () {
+    function createTest() {
+        waitForElm('header + div[data-csk-entry-type="hero"] > .MuiContainer-root > div[class*="HeroSubpage-content"]').then(function () {
+            document.querySelector("body").classList.add("spz-28002");
+
+            submitTestDetails('Variant_28001');
+        });
+
+        createCookie('spz-28002-loaded', 'true', 1);
+    }
+
+    function removeTest() {
+        document.body.classList.remove("spz-28002");
+    }
+
+    // Passing test details to hidden fields
+    function submitTestDetails(cro_test) {
+        if (document.querySelector('form.hs-form-private .hs_cro_test_2 .input .hs-input')) {
+            document.querySelector('form.hs-form-private .hs_cro_test_2 .input .hs-input').setAttribute('value', cro_test);
+        }
+    }
+
+    function demoChecked() {
+        const sInt = setInterval(() => {
+            var cro_field = document.querySelector('form.hs-form-private .hs_cro_test_2 .input .hs-input');
+
+            // Check if spz-28002-loaded cookie is present and cro_field is present
+            if ((cro_field && cro_field.val != '') && isCookieExist('spz-28002-loaded')) {
+                clearInterval(sInt);
+                submitTestDetails('Variant_28002');
+
+                deleteCookie('spz-28002-loaded');
+            }
+        }, 1000);
+
+        setTimeout(() => {
+            clearInterval(sInt);
+        }, 10000);
+    }
+
+    history.pushState = (function (f) {
+        return function pushState() {
+            var ret = f.apply(this, arguments);
+            window.dispatchEvent(new Event('pushstate'));
+            window.dispatchEvent(new Event('locationchange'));
+            return ret;
+        };
+    })(history.pushState);
+    history.replaceState = (function (f) {
+        return function replaceState() {
+            var ret = f.apply(this, arguments);
+            window.dispatchEvent(new Event('replacestate'));
+            window.dispatchEvent(new Event('locationchange'));
+            return ret;
+        };
+    })(history.replaceState);
+    window.addEventListener('popstate', function () {
+        window.dispatchEvent(new Event('locationchange'));
+    });
+    window.addEventListener('locationchange', function () {
+        url = location.href;
+        urlCheck(url);
+    });
+    var url = location.href;
+    urlCheck(url);
+
+    function urlCheck(url) {
+        let testURL = '';
+        if (window.location.pathname.indexOf("platform/startup") > -1) {
+            testURL = window.location.href;
+        }
+        if (isSameUrl(url, testURL, true)) {
+            createTest();
+        } else {
+            removeTest();
+        }
+        if (window.location.pathname.indexOf("/demo") > -1) {
+            // waitForElm('div[class*="MuiModal-root"] div[class*="Modal-styledReactPlayer"] video').then(function () {
+            //     if (document.querySelector('div[class*="MuiModal-root"] div[class*="Modal-styledReactPlayer"] video')) {
+            //         document.querySelector('div[class*="MuiModal-root"] div[class*="Modal-styledReactPlayer"] video').setAttribute('playsinline', '');
+            //     }
+            // });
+            demoChecked();
+        }
+    }
+
+    function isSameUrl(currentUrl, specifiedUrl, includeQueryParams) {
+        currentUrl = currentUrl.includes("#") ?
+            currentUrl.slice(0, currentUrl.indexOf("#")) :
+            currentUrl;
+        specifiedUrl = specifiedUrl.includes("#") ?
+            specifiedUrl.slice(0, specifiedUrl.indexOf("#")) :
+            specifiedUrl;
+        if (!includeQueryParams)
+            currentUrl = currentUrl.includes("?") ?
+                currentUrl.slice(0, currentUrl.indexOf("?")) :
+                currentUrl;
+        if (currentUrl === specifiedUrl || currentUrl === specifiedUrl + "/")
+            return true;
+        return false;
+    }
+
+    function waitForElm(selector) {
+        return new Promise(function (resolve) {
+            if (document.querySelector(selector)) {
+                return resolve(document.querySelector(selector));
+            }
+            const observer = new MutationObserver(function (mutations) {
+                if (document.querySelector(selector)) {
+                    resolve(document.querySelector(selector));
+                    observer.disconnect();
+                }
+            });
+            observer.observe(document, { attributes: true, childList: true, subtree: true, characterData: true });
+        });
+    }
+
+    // Create cookie
+    function createCookie(name, value, days) {
+        var expires = "";
+        if (days) {
+            var date = new Date();
+            date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+            expires = "; expires=" + date.toUTCString();
+        }
+        document.cookie = name + "=" + value + expires + "; path=/";
+    }
+
+    // Check if cookie exists
+    function isCookieExist(name) {
+        var nameEQ = name + "=";
+        var ca = document.cookie.split(';');
+        for (var i = 0; i < ca.length; i++) {
+            var c = ca[i];
+            while (c.charAt(0) == ' ') c = c.substring(1, c.length);
+            if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+        }
+        return null;
+    }
+
+    // Delete cookie
+    function deleteCookie(name) {
+        document.cookie = name + '=; expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+    }
+})();
