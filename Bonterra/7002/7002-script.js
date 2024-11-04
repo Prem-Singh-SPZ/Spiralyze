4b0f013d4a5c2112dbea81a2025763aa08c0867c
Prem-Singh-SPZ <prem@spiralyze.com>
2024-11-04 13:39:27 +0530

    7002

--- Bonterra/7002/7002-script.js
+++ Bonterra/7002/7002-script.js
@@ -102,6 +102,10 @@ function createTest() {
         });
 
         document.querySelector('#HERO .container .block-wrapper .block-2 > .mktoText > div .stats .stats__item:last-child .stats__item-footer img').setAttribute('src', 'https://488-ILM-190.mktoweb.com/rs/488-ILM-190/images/handshake-icon-periwinkle.webp');
+
+        //clone existing section to new place
+        let cloneSection = document.querySelector('body.spz-7002 #HERO .container .block-wrapper .block-2 > .mktoText > div .stats').cloneNode(true);
+        document.querySelector('body.spz-7002 #HERO .container .block-wrapper .block-3 > .mktoForm').insertAdjacentElement('afterend', cloneSection);
     });
 
     addTriage();
