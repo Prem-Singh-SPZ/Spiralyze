3fe146005245b6430a43ee946d1d7fc39ab407b6
Prem-Singh-SPZ <prem@spiralyze.com>
2024-11-22 16:16:52 +0530

    15001

--- Komodo/15001/15001-script.js
+++ Komodo/15001/15001-script.js
@@ -11,7 +11,7 @@ ready(function () {
   if (!document.querySelector('.spz-15001')) {
     document.querySelector('body').classList.add('spz-15001')
     //targeted page
-    if (window.location.href.indexOf('https://www.komodohealth.com/get-a-demo') > -1) {
+    if (window.location.href.indexOf('/demos') > -1) {
       const loadJS = (url, implementationCode, location) => {
         var scriptTag = document.createElement('script');
         scriptTag.src = url;
@@ -23,15 +23,6 @@ ready(function () {
       };
       function insertAfter(newNode, referenceNode) {
         referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
-      }
-      function outerHeight(el) {
-        const style = getComputedStyle(el);
-
-        return (
-          el.getBoundingClientRect().height +
-          parseFloat(style.marginTop) +
-          parseFloat(style.marginBottom)
-        );
       }
       function trigger(el, eventType) {
         if (typeof eventType === 'string' && typeof el[eventType] === 'function') {
@@ -45,15 +36,15 @@ ready(function () {
         }
       }
    
-      document.querySelector('body .demos-hero-content').insertAdjacentHTML('afterend', `<div id="new-formDemo-wrapper"><div class='lds-dual-ring'></div></div>`);
+      document.querySelector('body #main-content .demos-listing-hero').insertAdjacentHTML('afterend', `<div class="spz-hero-section"><div class="hero-section-container"><div class="hero-left-copy"><p class="eyebrow-title">MapView Market Landscape</p><h6 class="hero-headline">Get actionable insights in minutes</h6><p class="hero-desc">With an AI assistant and pre-built template dashboards, MapView empowers everyone to generate data analytics — even those with no technical or analytics expertise.</p><p class="hero-desc">Submit the form on the right for a custom demo.</p></div><div class="hero-form-wrapper"><div class="hero-form"><div class="spz-form-title">Get a Demo</div><div id="demo_Form_15001_wrapper"><div class='lds-dual-ring'></div></div></div></div></div></div>`);
 
 
 
       let i = 0;
       const CROInterval = setInterval(() => {
-        if (document.querySelector('.spz-15001 #new-formDemo-wrapper input[name="cro1"]')) {
+        if (document.querySelector('.spz-15001 #demo_Form_15001_wrapper input[name="cro1"]')) {
           clearInterval(CROInterval)
-          document.querySelector('.spz-15001 #new-formDemo-wrapper input[name="cro1"]').value = "#2001_variant"
+          document.querySelector('.spz-15001 #demo_Form_15001_wrapper input[name="cro1"]').value = "#2001_variant"
         }
         i++;
         if (i > 6) {
@@ -67,7 +58,7 @@ ready(function () {
             region: "na1",
             portalId: "6374024",
             formId: "bd5fd709-5849-4d8c-95a3-b52f5a1453c0",
-            target: '.spz-15001 #new-formDemo-wrapper',
+            target: '.spz-15001 #demo_Form_15001_wrapper',
             inlineMessage: "Thanks for submitting the form.",
             onFormSubmitted: function ($form) {
               console.log('Report conversion...');
@@ -79,64 +70,64 @@ ready(function () {
               if (document.querySelector('#popup_request_a_demo')) {
                 document.querySelector('#popup_request_a_demo').remove();
               }
-              const allFieldset = document.querySelectorAll('.spz-15001 #new-formDemo-wrapper .form-columns-2,.spz-15001 #new-formDemo-wrapper .form-columns-1,.spz-15001 #new-formDemo-wrapper .form-columns-0')
+              const allFieldset = document.querySelectorAll('.spz-15001 #demo_Form_15001_wrapper .form-columns-2,.spz-15001 #demo_Form_15001_wrapper .form-columns-1,.spz-15001 #demo_Form_15001_wrapper .form-columns-0')
               for (let i = 0; i < allFieldset.length; i++) {
                 allFieldset[i].replaceWith(...allFieldset[i].childNodes)
               }
               //long form
-              if (document.querySelector('.spz-15001 #new-formDemo-wrapper .hs_firstname')) {
-                document.querySelector('.spz-15001 #new-formDemo-wrapper .hs_firstname').classList.add("width50", "clear");
-                document.querySelector('.spz-15001 #new-formDemo-wrapper .hs_lastname').classList.add("width50", "with-margin");
-                document.querySelector('.spz-15001 #new-formDemo-wrapper .hs_company').classList.add("width50", "with-margin");
-                document.querySelector('.spz-15001 #new-formDemo-wrapper .hs_jobtitle').classList.add("width50", "clear");
-                document.querySelector('.spz-15001 #new-formDemo-wrapper .hs_email').classList.add("width50", "clear");
-                document.querySelector('.spz-15001 #new-formDemo-wrapper .hs-dependent-field').classList.add("clear");
-                document.querySelector('.spz-15001 #new-formDemo-wrapper .hs_phone').classList.add("width50", "with-margin");
-                document.querySelector('.spz-15001 #new-formDemo-wrapper .hs_consent_countries').classList.add("clear");
-                document.querySelector('.spz-15001 #new-formDemo-wrapper .hs-richtext').classList.add("clear", "pp-parent");
-                document.querySelector('.spz-15001 #new-formDemo-wrapper .hs_submit').classList.add("clear", "submit-parent");
-                insertAfter(document.querySelector('.spz-15001 #new-formDemo-wrapper .hs_email'), document.querySelector('.spz-15001 #new-formDemo-wrapper .hs_lastname'));
-                insertAfter(document.querySelector('.spz-15001 #new-formDemo-wrapper .hs_company'), document.querySelector('.spz-15001 #new-formDemo-wrapper .hs_email'));
-                insertAfter(document.querySelector('.spz-15001 #new-formDemo-wrapper .hs_jobtitle'), document.querySelector('.spz-15001 #new-formDemo-wrapper .hs_company'));
-                document.querySelector('.spz-15001 #new-formDemo-wrapper .hs_firstname label span').innerHTML = 'First Name';
-                document.querySelector('.spz-15001 #new-formDemo-wrapper .hs_lastname label span').innerHTML = 'Last Name';
-                document.querySelector('.spz-15001 #new-formDemo-wrapper .hs_email label span').innerHTML = 'Email';
-                document.querySelector('.spz-15001 #new-formDemo-wrapper .hs_company label span').innerHTML = 'Company';
-                document.querySelector('.spz-15001 #new-formDemo-wrapper .hs_phone label span').innerHTML = 'Phone';
-                document.querySelector('.spz-15001 #new-formDemo-wrapper .hs_jobtitle label span').innerHTML = 'Job Title (Optional)';
-                document.querySelector('.spz-15001 #new-formDemo-wrapper .hs_consent_countries label span').innerHTML = 'Country';
-                document.querySelector('.spz-15001 #new-formDemo-wrapper .hs_consent_countries select').value = 'United States'
-                trigger(document.querySelector('.spz-15001 #new-formDemo-wrapper .hs_consent_countries select'), 'change');
-                trigger(document.querySelector('.spz-15001 #new-formDemo-wrapper .hs_consent_countries select'), 'blur');
-                document.querySelector('.spz-15001 #new-formDemo-wrapper .hs_consent_countries select option:first-child').remove();
-                document.querySelector('.spz-15001 #new-formDemo-wrapper .hs_consent_countries').classList.add("hasValue");
-                document.querySelector('.spz-15001 #new-formDemo-wrapper .hs_submit input').value = "SUBMIT";
-                document.querySelector('.spz-15001 #new-formDemo-wrapper .hs_buying_timeline select').value = "Immediately";
-                trigger(document.querySelector('.spz-15001 #new-formDemo-wrapper .hs_buying_timeline select'), 'change');
-                document.querySelector('.spz-15001 #new-formDemo-wrapper .hs_consent_countries select').insertAdjacentHTML("afterend", "<div class='arrow-select'></div>")
-                const step1Checking = setInterval(function () {
-                  if (document.querySelector('.spz-15001 #new-formDemo-wrapper .hs_firstname input').value != "" &&
-                    document.querySelector('.spz-15001 #new-formDemo-wrapper .hs_lastname input').value != "" &&
-                    document.querySelector('.spz-15001 #new-formDemo-wrapper .hs_company input').value != "" &&
-                    document.querySelector('.spz-15001 #new-formDemo-wrapper .hs_email input').value != "") {
-                    clearInterval(step1Checking);
-                    document.querySelector('.spz-15001 #new-formDemo-wrapper').classList.add("step2")
-                  }
-                }, 200)
+              if (document.querySelector('.spz-15001 #demo_Form_15001_wrapper .hs_firstname')) {
+                document.querySelector('.spz-15001 #demo_Form_15001_wrapper .hs_firstname').classList.add("width50", "clear");
+                document.querySelector('.spz-15001 #demo_Form_15001_wrapper .hs_lastname').classList.add("width50", "with-margin");
+                document.querySelector('.spz-15001 #demo_Form_15001_wrapper .hs_company').classList.add("width50", "with-margin");
+                document.querySelector('.spz-15001 #demo_Form_15001_wrapper .hs_jobtitle').classList.add("width50", "clear");
+                document.querySelector('.spz-15001 #demo_Form_15001_wrapper .hs_email').classList.add("width50", "clear");
+                document.querySelector('.spz-15001 #demo_Form_15001_wrapper .hs-dependent-field').classList.add("clear");
+                document.querySelector('.spz-15001 #demo_Form_15001_wrapper .hs_phone').classList.add("width50", "with-margin");
+                document.querySelector('.spz-15001 #demo_Form_15001_wrapper .hs_consent_countries').classList.add("clear");
+                document.querySelector('.spz-15001 #demo_Form_15001_wrapper .hs-richtext').classList.add("clear", "pp-parent");
+                document.querySelector('.spz-15001 #demo_Form_15001_wrapper .hs_submit').classList.add("clear", "submit-parent");
+                insertAfter(document.querySelector('.spz-15001 #demo_Form_15001_wrapper .hs_email'), document.querySelector('.spz-15001 #demo_Form_15001_wrapper .hs_lastname'));
+                insertAfter(document.querySelector('.spz-15001 #demo_Form_15001_wrapper .hs_company'), document.querySelector('.spz-15001 #demo_Form_15001_wrapper .hs_email'));
+                insertAfter(document.querySelector('.spz-15001 #demo_Form_15001_wrapper .hs_jobtitle'), document.querySelector('.spz-15001 #demo_Form_15001_wrapper .hs_company'));
+                document.querySelector('.spz-15001 #demo_Form_15001_wrapper .hs_firstname label span').innerHTML = 'First Name';
+                document.querySelector('.spz-15001 #demo_Form_15001_wrapper .hs_lastname label span').innerHTML = 'Last Name';
+                document.querySelector('.spz-15001 #demo_Form_15001_wrapper .hs_email label span').innerHTML = 'Email';
+                document.querySelector('.spz-15001 #demo_Form_15001_wrapper .hs_company label span').innerHTML = 'Company';
+                document.querySelector('.spz-15001 #demo_Form_15001_wrapper .hs_phone label span').innerHTML = 'Phone';
+                document.querySelector('.spz-15001 #demo_Form_15001_wrapper .hs_jobtitle label span').innerHTML = 'Job Title (Optional)';
+                document.querySelector('.spz-15001 #demo_Form_15001_wrapper .hs_consent_countries label span').innerHTML = 'Country';
+                document.querySelector('.spz-15001 #demo_Form_15001_wrapper .hs_consent_countries select').value = 'United States'
+                trigger(document.querySelector('.spz-15001 #demo_Form_15001_wrapper .hs_consent_countries select'), 'change');
+                trigger(document.querySelector('.spz-15001 #demo_Form_15001_wrapper .hs_consent_countries select'), 'blur');
+                document.querySelector('.spz-15001 #demo_Form_15001_wrapper .hs_consent_countries select option:first-child').remove();
+                document.querySelector('.spz-15001 #demo_Form_15001_wrapper .hs_consent_countries').classList.add("hasValue");
+                document.querySelector('.spz-15001 #demo_Form_15001_wrapper .hs_submit input').value = "SUBMIT";
+                document.querySelector('.spz-15001 #demo_Form_15001_wrapper .hs_buying_timeline select').value = "Immediately";
+                trigger(document.querySelector('.spz-15001 #demo_Form_15001_wrapper .hs_buying_timeline select'), 'change');
+                document.querySelector('.spz-15001 #demo_Form_15001_wrapper .hs_consent_countries select').insertAdjacentHTML("afterend", "<div class='arrow-select'></div>")
+                // const step1Checking = setInterval(function () {
+                //   if (document.querySelector('.spz-15001 #demo_Form_15001_wrapper .hs_firstname input').value != "" &&
+                //     document.querySelector('.spz-15001 #demo_Form_15001_wrapper .hs_lastname input').value != "" &&
+                //     document.querySelector('.spz-15001 #demo_Form_15001_wrapper .hs_company input').value != "" &&
+                //     document.querySelector('.spz-15001 #demo_Form_15001_wrapper .hs_email input').value != "") {
+                //     clearInterval(step1Checking);
+                //     document.querySelector('.spz-15001 #demo_Form_15001_wrapper').classList.add("step2")
+                //   }
+                // }, 200)
               }
               //short form
               else {
-                if (document.querySelector('.spz-15001 #new-formDemo-wrapper .hs_email')) {
-                  document.querySelector('.spz-15001 #new-formDemo-wrapper .hs_email label span').innerHTML = 'Email';
-                  document.querySelector('.spz-15001 #new-formDemo-wrapper .hs_email').classList.add("clear");
+                if (document.querySelector('.spz-15001 #demo_Form_15001_wrapper .hs_email')) {
+                  document.querySelector('.spz-15001 #demo_Form_15001_wrapper .hs_email label span').innerHTML = 'Email';
+                  document.querySelector('.spz-15001 #demo_Form_15001_wrapper .hs_email').classList.add("clear");
                 }
-                if (document.querySelector('.spz-15001 #new-formDemo-wrapper .hs_buying_timeline select')) {
-                  document.querySelector('.spz-15001 #new-formDemo-wrapper .hs_buying_timeline select').value = "Immediately";
-                  trigger(document.querySelector('.spz-15001 #new-formDemo-wrapper .hs_buying_timeline select'), 'change');
+                if (document.querySelector('.spz-15001 #demo_Form_15001_wrapper .hs_buying_timeline select')) {
+                  document.querySelector('.spz-15001 #demo_Form_15001_wrapper .hs_buying_timeline select').value = "Immediately";
+                  trigger(document.querySelector('.spz-15001 #demo_Form_15001_wrapper .hs_buying_timeline select'), 'change');
                 }
-                document.querySelector('.spz-15001 #new-formDemo-wrapper .hs_submit input').value = "SUBMIT";
+                document.querySelector('.spz-15001 #demo_Form_15001_wrapper .hs_submit input').value = "SUBMIT";
               }
-              var allInput = document.querySelectorAll('.spz-15001 #new-formDemo-wrapper .hs-input');
+              var allInput = document.querySelectorAll('.spz-15001 #demo_Form_15001_wrapper .hs-input');
               function errorState(item) {
                 var i = 0;
                 var errorInterval = setInterval(() => {
@@ -179,18 +170,18 @@ ready(function () {
                 labelChange(e);
                 errorState(e);
               })
-              document.querySelector('.spz-15001 #new-formDemo-wrapper .hs_submit input').addEventListener("click", function () {
+              document.querySelector('.spz-15001 #demo_Form_15001_wrapper .hs_submit input').addEventListener("click", function () {
                 allInput.forEach(function (e) {
                   errorState(e);
                 })
-                if (document.querySelector('.spz-15001 #new-formDemo-wrapper .hs_consent_countries select')) {
-                  trigger(document.querySelector('.spz-15001 #new-formDemo-wrapper .hs_consent_countries select'), 'change');
+                if (document.querySelector('.spz-15001 #demo_Form_15001_wrapper .hs_consent_countries select')) {
+                  trigger(document.querySelector('.spz-15001 #demo_Form_15001_wrapper .hs_consent_countries select'), 'change');
                 }
                 let i = 0;
                 const submitInterval = setInterval(() => {
-                  if (document.querySelector('.spz-15001 #new-formDemo-wrapper .hs_submit input').value != "SUBMIT") {
+                  if (document.querySelector('.spz-15001 #demo_Form_15001_wrapper .hs_submit input').value != "SUBMIT") {
                     clearInterval(submitInterval)
-                    document.querySelector('.spz-15001 #new-formDemo-wrapper .hs_submit input').value = "SUBMIT";
+                    document.querySelector('.spz-15001 #demo_Form_15001_wrapper .hs_submit input').value = "SUBMIT";
                   }
                   i++;
                   if (i > 80) {
@@ -203,28 +194,6 @@ ready(function () {
         }, document.body);
       }
       loadDemoForm()
-    }
-    //non targeted page
-    else {
-      // let elementArray = [];
-      // for (let i = 0; i < document.querySelectorAll('a[href="#popup_request_a_demo"]').length; i++) {
-      //   elementArray[i] = document.querySelectorAll('a[href="#popup_request_a_demo"]')[i];
-      // }
-      // for (let i = 0; i < elementArray.length; i++) {
-      //   elementArray[i].setAttribute("href", "https://www.komodohealth.com/get-a-demo");
-      //   elementArray[i].removeAttribute("rel");
-      // }
-
-      let rfst = setInterval(() => {
-        document.querySelectorAll('a[href="#popup_request_a_demo"]').forEach(function (el) {
-          el.setAttribute("href", "https://www.komodohealth.com/get-a-demo");
-          el.removeAttribute("rel");
-        })
-      }, 100);
-
-      setTimeout(() => {
-        clearInterval(rfst);
-      }, 2000);
     }
   }
 })
