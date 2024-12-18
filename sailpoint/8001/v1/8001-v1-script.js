c401487cdbe9cacd6af68f79fd928f8fa8943648
Prem-Singh-SPZ <prem@spiralyze.com>
2024-12-18 12:41:58 +0530

    8001

--- sailpoint/8001/v1/8001-v1-script.js
+++ sailpoint/8001/v1/8001-v1-script.js
@@ -9,7 +9,7 @@ function createTest() {
 
 function formModify() {
     if (document.querySelector('#page-container .page-transition main #contact-us .row__inner > .column:last-child .column__inner') && !document.querySelector('.spz-form-title-wrapper')) {
-        document.querySelector('#page-container .page-transition main #contact-us .row__inner > .column:last-child .column__inner').insertAdjacentHTML('afterbegin', `<div class="spz-form-title-wrapper"><h6 class="f-title">Keep exploring SailPoint's identity security platform</h6><p class="f-desc">Thank you for exploring your first 3 product tours. For unlimited access to all product tours, simply provide your information below.</p></div>`);
+        document.querySelector('#page-container .page-transition main #contact-us .row__inner > .column:last-child .column__inner').insertAdjacentHTML('afterbegin', `<div class="spz-form-title-wrapper"><h6 class="f-title">Keep exploring SailPoint's identity security platform</h6><p class="f-desc">Thank you for exploring your first 3 product tours. For unlimited access to all product tours, simply provide your information below.</p><a class="close-modal" href="javascript:;"><img src="//res.cloudinary.com/spiralyze/image/upload/v1734504904/sailpoint/8001/close_icons.svg"></a></div>`);
     }
 
     document.querySelectorAll('#mktoForm_1018.mktoForm .mktoFormRow').forEach(function (elm) {
@@ -20,7 +20,17 @@ function formModify() {
 
     document.querySelector('.row_FirstName').insertAdjacentElement('beforebegin', document.querySelector('.row_Email'));
     document.querySelector('.mkto-wrap .mktoForm .mktoButtonRow .mktoButtonWrap .mktoButton').textContent = 'Unlock all product tours';
+
+    // document.querySelector('#contact-us').style.top = document.querySelector('nav.navigation').offsetHeight + 'px';
+
 }
+
+//on window scroll, check the height of nav.navigation and assign it as top value to the #contact-us
+// window.addEventListener('scroll', function () {
+//     if (document.querySelector('.navigation')) {
+//         document.querySelector('#contact-us').style.top = document.querySelector('nav.navigation').offsetHeight + 'px';
+//     }
+// });
 
 window.addEventListener('click', function (e) {
     // console.log(e.target);
@@ -29,8 +39,17 @@ window.addEventListener('click', function (e) {
 
         anchorCount++;
         if (anchorCount >= 4) {
-            document.querySelector('.mktoForm').style.display = 'block';
+            e.preventDefault();
+            document.body.classList.add('spz-modal-open');
+            document.querySelector('html').classList.add('spz-modal-open');
+            // document.querySelector('#contact-us').style.top = document.querySelector('nav.navigation').offsetHeight + 'px';
         }
+    }
+
+    //check if .close-modal is clicked
+    if (e.target.classList.contains('close-modal')) {
+        document.body.classList.remove('spz-modal-open');
+        document.querySelector('html').classList.remove('spz-modal-open');
     }
 });
 
