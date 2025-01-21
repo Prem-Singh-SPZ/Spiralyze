703feeb62c25b947e87b103ffbabbb4a3f94e4fe
Prem-Singh-SPZ <prem@spiralyze.com>
2025-01-21 18:40:00 +0530

    1001

--- Mexio/1001/1001-script.js
+++ Mexio/1001/1001-script.js
@@ -1,52 +1,80 @@
-//demo page
-const currentURL = window.location.href.split('?')[0]; // Get the base URL without parameters
-if (currentURL === "https://www2.maxio.com/l/699023/2022-08-16/nh4lk") {
-	var iframebodyEle = document.querySelector('body');
-	iframebodyEle.classList.add('spz-1001-iframe');
-	waitForElm('.spz-1001-iframe form#pardot-form .submit input').then(function () {
-		hiddenValue();
+// iFrame Code
+function formPage() {
+	var iframebodyInterval = setInterval(function () {
+		var iframebodyEle = document.querySelector('body');
+		if (iframebodyEle) {
+			if (!iframebodyEle.classList.contains('spz-1001-iframe')) {
+				clearInterval(iframebodyInterval);
+				iframebodyEle.classList.add('spz-1001-iframe');
+				waitForElm('body #pardot-form').then(function () {
+					var formAction = document.querySelector('body #pardot-form').action;
+					//check if url has query params
+					if (formAction.includes('?')) {
+						document.querySelector('body #pardot-form').action = formAction + '&spz=1001';
+					} else {
+						document.querySelector('body #pardot-form').action = formAction + '?spz=1001';
+					}
+				});
+				waitForElm('.spz-1001-iframe form#pardot-form .submit input').then(function () {
+					hiddenValue();
+				});
+			}
+		}
 	});
 }
 
-let checkBody = setInterval(function () {
-	if (document.querySelectorAll('body').length > 0) {
-		clearInterval(checkBody);
-		load_1001();
-	}
-});
-function load_1001() {
-	var bodyEle = document.querySelector('body');
-	if (!bodyEle.classList.contains('spz-1001')) {
-		bodyEle.classList.add('spz-1001');
+// Demo Page Code
+function demoPage() {
+	var bodyInterval = setInterval(function () {
+		var bodyEle = document.querySelector('body');
+		if (!bodyEle.classList.contains('spz-1001-demo')) {
+			clearInterval(bodyInterval);
+			bodyEle.classList.add('spz-1001-demo');
+			waitForElm('.blocks-page iframe').then(function () {
+				setTimeout(() => {
+					var iframeSrc = document.querySelector('.blocks-page iframe').src;
+					if (iframeSrc.includes('?')) {
+						document.querySelector('.blocks-page iframe').src = iframeSrc + '&spz=1001';
+					} else {
+						document.querySelector('.blocks-page iframe').src = iframeSrc + '?spz=1001';
+					}
+				}, 500);
+			});
+		}
+	});
+}
 
-		//home page
-		if (window.location.pathname === "/") {
-			bodyEle.classList.add('spz-home');
-			//hidden value
+// Main Page Code
+function mainPage() {
+	var mainbodyInterval = setInterval(function () {
+		var mainbodyEle = document.querySelector('body');
+		if (!mainbodyEle.classList.contains('spz-1001')) {
+			clearInterval(mainbodyInterval);
+			mainbodyEle.classList.add('spz-1001');
 			cookieValue('#1001', '#1001_variant1');
 
 			waitForElm('#content #blocks-page .hero-generic .container > div > div:first-child .my-6 a').then(function () {
 				document.querySelector('#content #blocks-page .hero-generic .container > div > div:first-child').insertAdjacentHTML('beforeend', `<div class="spz-hero">
-					<h1 class="spz-headline">Grow Recurring Revenue</h1>
-					<p class="sub-headline">by optimizing subscription & contract billing</p>
-					<ul class="hero-list">
-						<li><span>Billing.</span> Manage term, evergreen, and usage subscriptions. Automate invoicing. Handle payments, trials, sign-ups, add-ons, and upsells.</li>
-						<li><span>Revenue Recognition.</span> Handle GAAP & IFRS. Build, compare, and customize multiple schedules. Get drill-down reports and detailed audit trails.</li>
-						<li><span>Reporting.</span> One-click reports for ARR, MRR, churn, renewals, and more. Advanced cohort, revenue waterfalls, and drill-down reports.</li>
-					</ul>
-					<div class="spz-email">
-						 <div class="form-group-wrapper">
-                                <div class="form-group-spz">
-                                    <input type="email" name="email" class="form-field-spz email-hero-spz" placeholder=" " />
-                                    <label class="form-label-spz">Email</label>
-                                </div>
-                                <a href="/demo" id="hero-copy-url" class="hero-btn redirect-to-demo spz_tracking_1001">
-                                    <span>Get a Demo</span> 
-                                </a>
-                        </div>
+				<h1 class="spz-headline">Grow Recurring Revenue</h1>
+				<p class="sub-headline">by optimizing subscription & contract billing</p>
+				<ul class="hero-list">
+					<li><span>Billing.</span> Manage term, evergreen, and usage subscriptions. Automate invoicing. Handle payments, trials, sign-ups, add-ons, and upsells.</li>
+					<li><span>Revenue Recognition.</span> Handle GAAP & IFRS. Build, compare, and customize multiple schedules. Get drill-down reports and detailed audit trails.</li>
+					<li><span>Reporting.</span> One-click reports for ARR, MRR, churn, renewals, and more. Advanced cohort, revenue waterfalls, and drill-down reports.</li>
+				</ul>
+				<div class="spz-email">
+					 <div class="form-group-wrapper">
+							<div class="form-group-spz">
+								<input type="email" name="email" class="form-field-spz email-hero-spz" placeholder=" " />
+								<label class="form-label-spz">Email</label>
+							</div>
+							<a href="/demo" id="hero-copy-url" class="hero-btn redirect-to-demo spz_tracking_1001">
+								<span>Get a Demo</span> 
+							</a>
 					</div>
 				</div>
-				`);
+			</div>
+			`);
 
 				//insert a banner section with 4 logos 
 				document.querySelector('#content #blocks-page .hero-generic .container').insertAdjacentHTML('afterend', `<div class="spz-banner"><div class="spz-banner-wrapper"><div class="b-logo"><img src="//res.cloudinary.com/spiralyze/image/upload/v1731513060/maxio/1001/chily_piper_logo.svg" alt="Chili Piper"></div><div class="b-logo"><img src="//res.cloudinary.com/spiralyze/image/upload/v1731513060/maxio/1001/mailgun_logo.svg" alt="Mailgun"></div><div class="b-logo"><img src="//res.cloudinary.com/spiralyze/image/upload/v1731513060/maxio/1001/stack_overflow_logo.svg" alt="Stack overflow"></div><div class="b-logo"><img src="//res.cloudinary.com/spiralyze/image/upload/v1731513060/maxio/1001/jasper_logo.svg" alt="Jasper"></div></div></div>`);
@@ -54,47 +82,22 @@ function load_1001() {
 				//on click of .redirect-to-demo store whatever value is in the input field in cookie 
 				document.querySelector('.redirect-to-demo').addEventListener('click', function () {
 					var emailValue = document.querySelector('.email-hero-spz').value;
-					// store this value in session storage
-					sessionStorage.setItem('emailValue', emailValue);
+					emailValue = emailValue.replace(/\+/g, '%2B');
+					setCookie('SPZ_Carry_Email',emailValue,1);
 				});
 			});
 		}
-
-		//demo page
-		if (window.location.pathname === "/demo") {
-			bodyEle.classList.add('spz-1001-demo');
-
-			waitForElm('.spz-1001-demo iframe[src="https://www2.maxio.com/l/699023/2022-08-16/nh4lk"]').then(function () {
-				//set the value of email input field from session storage as the query param value for src of iframe
-				var emailValue = sessionStorage.getItem('emailValue');
-
-				//if there is + sign in the email value, replace it all with %2B
-				emailValue = emailValue.replace(/\+/g, '%2B');
-
-				//check if email value is not null
-				if (emailValue) {
-					var iframeSrc = document.querySelector('.spz-1001-demo iframe[src="https://www2.maxio.com/l/699023/2022-08-16/nh4lk"]').src;
-					document.querySelector('.spz-1001-demo iframe[src="https://www2.maxio.com/l/699023/2022-08-16/nh4lk"]').src = iframeSrc + '?email=' + emailValue;
-				}
-			});
-		}
-	}
+	});
 }
 
-//wait for element
-function waitForElm(selector) {
-	return new Promise(function (resolve) {
-		if (document.querySelector(selector)) {
-			return resolve(document.querySelector(selector));
-		}
-		const observer = new MutationObserver(function (mutations) {
-			if (document.querySelector(selector)) {
-				resolve(document.querySelector(selector));
-				observer.disconnect();
-			}
-		});
-		observer.observe(document, { attributes: true, childList: true, subtree: true, characterData: true });
-	});
+if (window.location.pathname === "/") {
+	mainPage();
+}
+if (location.href.indexOf('/demo') > -1) {
+	demoPage();
+}
+if (location.href.indexOf('spz=1001') > -1) {
+	formPage();
 }
 
 function hiddenValue() {
@@ -106,6 +109,19 @@ function hiddenValue() {
 			cro_primary.value = ExistingHiddenFieldValue;
 		}
 	});
+
+	var spz_email_Interval = setInterval(function () {
+		var emailfiled = document.querySelector('form#pardot-form .form-field.CP_Email input');
+			if(emailfiled){
+		  clearInterval(spz_email_Interval);
+		  var SPZEmailValue = getCookie('SPZ_Carry_Email');
+		  var emailField = document.querySelector('form#pardot-form .form-field.CP_Email input');
+		  if (SPZEmailValue) {
+			emailField.value = SPZEmailValue;
+			emailField.focus();
+		  }
+		}
+	  });
 }
 
 function cookieValue(currentHiddenFieldName, currentHiddenFieldValue) {
@@ -142,6 +158,18 @@ function getCookie(name) {
 	return null;
 }
 
-function delete_cookie(name) {
-	document.cookie = name + '=; expires=Thu, 01 Jan 1970 00:00:01 GMT; path=/; domain=maxio.com;';
+// Generic Code
+function waitForElm(selector) {
+	return new Promise(function (resolve) {
+		if (document.querySelector(selector)) {
+			return resolve(document.querySelector(selector));
+		}
+		const observer = new MutationObserver(function (mutations) {
+			if (document.querySelector(selector)) {
+				resolve(document.querySelector(selector));
+				observer.disconnect();
+			}
+		});
+		observer.observe(document, { attributes: true, childList: true, subtree: true, characterData: true });
+	});
 }
