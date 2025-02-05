871ec085f5be573545bc60fc5f3f7022aad6349a
Prem-Singh-SPZ <prem@spiralyze.com>
2025-02-05 21:04:54 +0530

    14001

--- Mexio/14001/code.js
+++ Mexio/14001/code.js
@@ -4,7 +4,7 @@ const asana_URL = `https://app.asana.com/0/1202434538289854/1209285828853568`
 const template_formElements = {
   customHTMLBefore: `
 	${ /*This html goes is inserted before the form */'' }
-    <div class="spz-form-heading">Show me Demo</div>
+    <div class="spz-form-heading">Maxio Billing Sandbox</div>
   `,
   customHTMLAfter: ``,
 };
@@ -19,7 +19,7 @@ const template_inputsSelectors = ["#signup_first_name", "#signup_last_name", "#s
 const template_labelValues = ["First Name", "Last Name", "Work Email", "Organization", "Role"] 
 
 const formLoaded = setInterval(() => {
-  if(document.querySelector(template_formUniqueSelector) && document.querySelectorAll(`${template_formUniqueSelector} input`).length > 0){
+  if(document.querySelector(template_formUniqueSelector) && document.querySelectorAll(`${template_formUniqueSelector} select`).length > 0){
     clearInterval(formLoaded);
     addForm(template_formElements, template_formUniqueSelector);
     animateLabels(template_inputsSelectors, template_labelValues)
@@ -163,4 +163,71 @@ function addForm(formData, formSelector) {
       }
     });
   }
+}
+
+
+function hiddenValue() {
+  var spz_cro_Interval = setInterval(function () {
+      var cro_primary = document.querySelector('form#pardot-form .form-field.cro_primary input');
+      if (cro_primary) {
+          clearInterval(spz_cro_Interval);
+          var ExistingHiddenFieldValue = getCookie('HiddenFieldValue');
+          cro_primary.value = ExistingHiddenFieldValue;
+      }
+  });
+}
+
+function cookieValue(currentHiddenFieldName, currentHiddenFieldValue) {
+  var ExistingHiddenFieldName = getCookie('HiddenFieldName');
+  var ExistingHiddenFieldValue = getCookie('HiddenFieldValue');
+
+  if (!ExistingHiddenFieldName) {
+      setCookie('HiddenFieldName', currentHiddenFieldName, 1);
+      setCookie('HiddenFieldValue', currentHiddenFieldValue, 1);
+  } else if (ExistingHiddenFieldName && !ExistingHiddenFieldName.includes(currentHiddenFieldName) && !ExistingHiddenFieldValue.includes(currentHiddenFieldValue)) {
+      setCookie('HiddenFieldName', ExistingHiddenFieldName + ',' + currentHiddenFieldName, 1);
+      setCookie('HiddenFieldValue', ExistingHiddenFieldValue + ',' + currentHiddenFieldValue, 1);
+  }
+}
+
+function setCookie(name, value, days) {
+  var expires = "";
+  if (days) {
+      var date = new Date();
+      date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+      expires = "; expires=" + date.toUTCString();
+  }
+  document.cookie = name + "=" + (value || "") + expires + "; path=/; domain=maxio.com;";
+}
+
+function getCookie(name) {
+  var nameEQ = name + "=";
+  var ca = document.cookie.split(';');
+  for (var i = 0; i < ca.length; i++) {
+      var c = ca[i];
+      while (c.charAt(0) == ' ') c = c.substring(1, c.length);
+      if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+  }
+  return null;
+}
+
+// Delete cookie
+function deleteCookie(name) {
+  document.cookie = name + '=; expires=Thu, 01 Jan 1970 00:00:01 GMT; path=/; domain=maxio.com;';
+}
+
+// Generic Code
+function waitForElm(selector) {
+  return new Promise(function (resolve) {
+      if (document.querySelector(selector)) {
+          return resolve(document.querySelector(selector));
+      }
+      const observer = new MutationObserver(function (mutations) {
+          if (document.querySelector(selector)) {
+              resolve(document.querySelector(selector));
+              observer.disconnect();
+          }
+      });
+      observer.observe(document, { attributes: true, childList: true, subtree: true, characterData: true });
+  });
 }
