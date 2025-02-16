809684f2efd23a6767d5a72fdacae9d9c1371883
Prem-Singh-SPZ <prem@spiralyze.com>
2025-02-16 21:04:05 +0530

    1005

--- sailpoint/1005/1005-Script.js
+++ sailpoint/1005/1005-Script.js
@@ -38,7 +38,7 @@
               socialProofImages: [
                 { url: "//res.cloudinary.com/spiralyze/image/upload/v1730174324/sailpoint/1001/logo_01.svg", imgAlt: "GM" },
                 { url: "//res.cloudinary.com/spiralyze/image/upload/v1730174324/sailpoint/1001/logo_02.svg", imgAlt: "Hershey" },
-                { url: "//res.cloudinary.com/spiralyze/image/upload/v1730174324/sailpoint/1001/logo_03.svg", imgAlt: "Paccar"},
+                { url: "//res.cloudinary.com/spiralyze/image/upload/v1730174324/sailpoint/1001/logo_03.svg", imgAlt: "Paccar" },
                 { url: "//res.cloudinary.com/spiralyze/image/upload/v1730174324/sailpoint/1001/logo_04.svg", imgAlt: "Philips" },
                 { url: "//res.cloudinary.com/spiralyze/image/upload/v1730174324/sailpoint/1001/logo_05.svg", imgAlt: "RWE" },
                 { url: "//res.cloudinary.com/spiralyze/image/upload/v1730174324/sailpoint/1001/logo_06.svg", imgAlt: "T Mobile" },
@@ -55,18 +55,18 @@
                   <div class="content-section">
                     ${formData.siteLogo.length !== 0 ? `<a class="site-logo" data-sentry-element="NextLink" data-sentry-source-file="MainLargeNavigation.tsx" href="/">
                       <img src="${formData.siteLogo}" alt="${formData.siteLogoAlt}" />
-                    </a>` : "" }
+                    </a>` : ""}
                     ${formData.contentSuperHeading.replace(/\s/g, "").length !== 0 ? `<div class="content-superheading">${formData.contentSuperHeading}</div>` : ""}
                     ${formData.contentHeading.replace(/\s/g, "").length !== 0 ? `<div class="content-heading">${formData.contentHeading}</div>` : ""}
                     <div class="spz-features-wrap">
                       ${formData.features.length > 0 ? formData.features.map((item) => {
-                        return `<div class="feature-item">
+              return `<div class="feature-item">
                           <img src="${item.checkmarkImage}" alt="checkmark" />
                           <span>${item.featureItemContent}</span>
                         </div>`;
-                      }).join("") : ""}
+            }).join("") : ""}
                     </div>
-                    ${formData.formArrow.length !== 0 ? `<img src="${formData.formArrow}" class="form-arrow" alt="Arrow" />` : "" }
+                    ${formData.formArrow.length !== 0 ? `<img src="${formData.formArrow}" class="form-arrow" alt="Arrow" />` : ""}
                   </div>
                   <div class="spz-form-section">
                     ${formData.formHeading.replace(/\s/g, "").length !== 0 ? `<div class="form-heading">${formData.formHeading}</div>` : ""}
@@ -78,10 +78,10 @@
                 <div class="social-proof-heading">${additionalSection.socialProofLogos.socialProofHeading}</div>
                 <div class="social-proof-images">
                   ${additionalSection.socialProofLogos.socialProofImages.length > 0 ? additionalSection.socialProofLogos.socialProofImages.map((item, index) => {
-                    return `<img src="${item.url}" alt="${item.imgAlt}">`;
-                  }).join("") + additionalSection.socialProofLogos.socialProofImages.map((item, index) => {
-                    return `<img src="${item.url}" class="repeat-img" alt="${item.imgAlt}">`;
-                  }).join("") : ""}
+              return `<img src="${item.url}" alt="${item.imgAlt}">`;
+            }).join("") + additionalSection.socialProofLogos.socialProofImages.map((item, index) => {
+              return `<img src="${item.url}" class="repeat-img" alt="${item.imgAlt}">`;
+            }).join("") : ""}
                 </div>
               </div>` : ``}
             </section>`;
@@ -139,7 +139,7 @@
             var phone_field = document.querySelector('.SPZ-1005 form.mktoForm .field-5');
             var job_field = document.querySelector('.SPZ-1005 form.mktoForm .field-7');
             job_field.after(phone_field);
-            
+
             var employees_field = document.querySelector('.SPZ-1005 form.mktoForm .field-8');
             var state_field = document.querySelector('.SPZ-1005 form.mktoForm .field-10');
             state_field.after(employees_field);
@@ -156,21 +156,9 @@
               }, 1000);
             });
 
-            function checkEmail() {
-              const emailField = document.querySelector('.SPZ-1005 .spz-form-section form.mktoForm input[name="Email"]');
-              const emailRegex = /^[^\s@]+@[a-zA-Z0-9]+(?:\.[a-zA-Z0-9-]+)*\.[a-zA-Z]{2,}$/;
-              if (emailField) {
-                if (emailField.value.trim() === '' || !emailRegex.test(emailField.value.trim())) {
-                  if(!emailField.parentElement.querySelector('.customError')){
-                    emailField.insertAdjacentHTML('afterend', '<div class="mktoError customError"><div class="mktoErrorMsg">valid email required.</div></div>');
-                  }
-                }
-              }
-            }
-
             function focusFields() {
               const form = document.querySelector('.SPZ-1005 form.mktoForm');
-              let emailFocusCount = 0; 
+              let emailFocusCount = 0;
               form.addEventListener('focus', function (event) {
                 const el = event.target;
                 if (el.classList.contains('mktoField')) {
@@ -178,15 +166,15 @@
                   checkAllFields();
                 }
                 var email_field = document.querySelector('.SPZ-1005 .spz-form-section form.mktoForm input[name="Email"]');
-                if(email_field.value.trim() === '') {
+                if (email_field.value.trim() === '') {
                   email_field.closest('.mktoFieldWrap').classList.remove('emailerror');
                 }
-                if(el.getAttribute('name') === 'Email' && !document.body.classList.contains('form-expand')) {
+                if (el.getAttribute('name') === 'Email' && !document.body.classList.contains('form-expand')) {
                   var checkerrorcnt = 0;
-                  var checkerrror = setInterval(function(){
-                    if(el.closest('.mktoFieldWrap') !== null){
+                  var checkerrror = setInterval(function () {
+                    if (el.closest('.mktoFieldWrap') !== null) {
                       clearInterval(checkerrror);
-                      if(el.closest('.mktoFieldWrap').querySelector('.mktoError:not(.customError)') !== null && el.closest('.mktoFieldWrap').querySelector('.mktoError:not(.customError)').style.display != 'none'){
+                      if (el.closest('.mktoFieldWrap').querySelector('.mktoError:not(.customError)') !== null && el.closest('.mktoFieldWrap').querySelector('.mktoError:not(.customError)').style.display != 'none') {
                         el.closest('.mktoFieldWrap').classList.add('emailerror');
                         checkEmail();
                       }
@@ -205,7 +193,7 @@
               }, true);
 
               form.addEventListener('keyup', function (event) {
-                if(!document.body.classList.contains('form-expand')){
+                if (!document.body.classList.contains('form-expand')) {
                   const el = event.target;
                   const fieldName = el.getAttribute('name');
 
@@ -229,7 +217,7 @@
                       document.body.classList.add('form-expand');
                     }
                   }
-                } 
+                }
               }, true);
 
               form.addEventListener('blur', function (event) {
@@ -239,7 +227,7 @@
                   checkAllFields();
                 }
 
-                if(!document.body.classList.contains('form-expand')){
+                if (!document.body.classList.contains('form-expand')) {
                   const el = event.target;
                   const fieldName = el.getAttribute('name');
 
@@ -264,7 +252,7 @@
                       document.body.classList.add('form-expand');
                     }
                   }
-                } 
+                }
               }, true);
 
               form.addEventListener('input', function (event) {
@@ -454,7 +442,7 @@
           const fieldWrap = field.closest('.mktoFieldWrap');
           if (fieldWrap) {
             // Check for error
-            const errorElement = fieldWrap.querySelector('.mktoError');
+            const errorElement = fieldWrap.querySelector('.mktoError:not(.customError)');
             if (errorElement && errorElement.style.display !== 'none') {
               fieldWrap.classList.add('error');
             } else {
@@ -476,6 +464,32 @@
         clearInterval(timeBuffer);
       }, 1000);
 
+      if (!document.body.classList.contains('form-expand')) {
+        const el = document.querySelector('input[name="Email"]');
+        const fieldName = el.getAttribute('name');
+        if (fieldName === 'Email') {
+          const newemailValue = el.value.trim();
+          const newemailRegex = /^[^\s@]+@[a-zA-Z0-9]+(?:\.[a-zA-Z0-9-]+)*\.[a-zA-Z]{2,}$/;
+          if (newemailValue == '' || !newemailRegex.test(newemailValue)) {
+            el.closest('.mktoFieldWrap').classList.add('emailerror');
+            checkEmail();
+          } else {
+            el.closest('.mktoFieldWrap').classList.remove('emailerror');
+          }
+        }
+
+        const firstName = document.querySelector('input[name="FirstName"]').value.trim();
+        const lastName = document.querySelector('input[name="LastName"]').value.trim();
+        const emailValue = document.querySelector('input[name="Email"]').value.trim();
+        const companyValue = document.querySelector('input[name="Company"]').value.trim();
+        const emailRegex = /^[^\s@]+@[a-zA-Z0-9]+(?:\.[a-zA-Z0-9-]+)*\.[a-zA-Z]{2,}$/;
+        if (['FirstName', 'LastName', 'Email', 'Company'].includes(fieldName)) {
+          if (firstName && lastName && companyValue && emailRegex.test(emailValue)) {
+            document.body.classList.add('form-expand');
+          }
+        }
+      }
+
       //inject current time and date in EST timezone into .intellimize2 hidden field
       var d = new Date();
       var n = d.toLocaleString('en-US', { timeZone: 'America/New_York' });
@@ -484,6 +498,20 @@
         int2.value = n;
     }
   });
+
+  function checkEmail() {
+    const emailField = document.querySelector('.SPZ-1005 .spz-form-section form.mktoForm input[name="Email"]');
+    const emailRegex = /^[^\s@]+@[a-zA-Z0-9]+(?:\.[a-zA-Z0-9-]+)*\.[a-zA-Z]{2,}$/;
+    if (emailField) {
+      if (emailField.value.trim() === '' || !emailRegex.test(emailField.value.trim())) {
+        if (!emailField.parentElement.querySelector('.customError')) {
+          if (emailField.closest('.mktoFieldWrap').querySelectorAll('.customError').length == 0) {
+            emailField.insertAdjacentHTML('afterend', '<div class="mktoError_1 customError"><div class="mktoErrorMsg">valid email required.</div></div>');
+          }
+        }
+      }
+    }
+  }
 
   // Do not touch below hidden field code for any Experiment over
 
