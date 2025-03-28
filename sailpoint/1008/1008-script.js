43390d06867e0a4b4f4ffde946fc039c6ca8b8f8
Prem-Singh-SPZ <prem@spiralyze.com>
2025-03-28 16:26:16 +0530

    Add mutation observer for error handling in form submission in 1008 module

--- sailpoint/1008/1008-script.js
+++ sailpoint/1008/1008-script.js
@@ -544,6 +544,45 @@
                 }
             }
 
+            waitForElm('.spz_1008 .mktoForm .mktoError').then(function (elm) {
+                if (elm.parentNode.querySelector('#ValidMsgEmail')) {
+                  const targetNode = elm.parentNode;
+                  const config = { attributes: true, childList: true, subtree: true };
+                  const callback = (mutationList, observer) => {
+                    for (const mutation of mutationList) {
+                      if (mutation.type === "childList") {
+                        if (elm.parentNode === null && elm.style.display != 'none') {
+                          targetNode.classList.add('error');
+                        } else {
+                          elm.parentNode.classList.add('error');
+                        }
+                        observer.disconnect();
+                      } else if (mutation.type === "attributes") {
+                        if (elm.parentNode === null) {
+                          targetNode.classList.add('error');
+                        } else {
+                          elm.parentNode.classList.add('error');
+                        }
+                        observer.disconnect();
+                      }
+                    }
+                  };
+                  const observer = new MutationObserver(callback);
+                  observer.observe(targetNode, config);
+                } else {
+                  let counterA = 0;
+                  const intervalIdA = setInterval(() => {
+                    if (document.querySelector('.spz_1008 .mktoForm .mktoError #ValidMsgEmail') !== null) {
+                      document.querySelector('.spz_1008 .mktoForm .mktoError #ValidMsgEmail').parentNode.parentNode.classList.add('error');
+                    }
+                    counterA++;
+                    if (counterA >= 10) {
+                      clearInterval(intervalIdA);
+                    }
+                  }, 500);
+                }
+              });
+
             //inject current time and date in EST timezone into .intellimize2 hidden field
             var d = new Date();
             var n = d.toLocaleString('en-US', { timeZone: 'America/New_York' });
