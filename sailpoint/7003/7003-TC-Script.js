ca7ae31ae7079412c8dbcdcc65f504fa66e66b55
Prem-Singh-SPZ <prem@spiralyze.com>
2025-06-05 16:53:13 +0530

    Add styles and scripts for SPZ 7003 modal functionality
    - Introduced a new CSS file (7003-Style.css) with styles for input autofill, modal transitions, and form elements.
    - Implemented JavaScript (7003-TC-Script.js) to manage modal behavior, including event listeners for navigation and form submissions.
    - Added functionality to handle cookies and hidden fields for tracking user interactions.
    - Enhanced responsiveness for mobile views and ensured accessibility features are in place.

---
+++ sailpoint/7003/7003-TC-Script.js
@@ -0,0 +1,214 @@
+
+(function () {
+
+  function createTest() {
+    let bodyLoaded = setInterval(function () {
+      const body = document.querySelector('body');
+      if (body) {
+        clearInterval(bodyLoaded);
+        if (!document.body.classList.contains('SPZ_7003_TC')) {
+          document.body.classList.add('SPZ_7003_TC');
+
+          //click event listener
+          document.querySelector('.SPZ_7003_TC #page-container nav.navigation .navigation__items + div a.btn[href*="/contact-us"]').addEventListener('click', function (e) {
+            sessionStorage.setItem('#7003_Contact', 'true');
+          });
+        }
+      }
+    });
+  }
+
+  function contact() {
+    let bodyLoaded = setInterval(function () {
+      const body = document.querySelector('body');
+      if (body) {
+        clearInterval(bodyLoaded);
+        if (!document.body.classList.contains('SPZ_7003_TC_Contact')) {
+          document.body.classList.add('SPZ_7003_TC_Contact');
+          const contactTrigger = sessionStorage.getItem('#7003_Contact');
+          if (contactTrigger) {
+            sessionStorage.removeItem('#7003_Contact');
+            removeSpecificCookieValue('SPZ_7003', 'SPZ_7003_variant');
+            hiddenValue('SPZ_7003', 'SPZ_7003_truecontrol');
+          }
+        }
+      }
+    });
+  }
+
+  function removeTest() {
+    setTimeout(() => {
+      if (document.querySelector('.SPZ_7003_TC')) {
+        document.body.classList.remove("SPZ_7003_TC");
+      }
+      if (document.querySelector('.SPZ_7003_TC_Contact')) {
+        document.body.classList.remove("SPZ_7003_TC_Contact");
+      }
+    }, 200);
+  }
+
+  history.pushState = (function (f) {
+    return function pushState() {
+      var ret = f.apply(this, arguments);
+      window.dispatchEvent(new Event("pushstate"));
+      window.dispatchEvent(new Event("locationchange"));
+      return ret;
+    };
+  })(history.pushState);
+  history.replaceState = (function (f) {
+    return function replaceState() {
+      var ret = f.apply(this, arguments);
+      window.dispatchEvent(new Event("replacestate"));
+      window.dispatchEvent(new Event("locationchange"));
+      return ret;
+    };
+  })(history.replaceState);
+  window.addEventListener("popstate", function () {
+    window.dispatchEvent(new Event("locationchange"));
+  });
+
+
+  // List of URLs
+  const urls = [
+    "https://www.sailpoint.com/",
+    "https://www.sailpoint.com/products/identity-security-cloud/atlas/suites",
+    "https://www.sailpoint.com/products/identity-security-software/identity-iq",
+    "https://www.sailpoint.com/products/identity-security-cloud/atlas/add-ons/cloud-infrastructure-entitlement-management",
+    "https://www.sailpoint.com/products/identity-security-cloud/atlas/add-ons/non-employee-risk-management",
+    "https://www.sailpoint.com/products/identity-security-cloud/atlas/add-ons/data-access-security",
+    "https://www.sailpoint.com/products/identity-security-cloud/atlas/add-ons/password-management",
+    "https://www.sailpoint.com/products/identity-security-cloud/atlas/add-ons/access-risk-management",
+    "https://www.sailpoint.com/products/connectivity/application-onboarding",
+    "https://www.sailpoint.com/products/identity-security-cloud/atlas",
+    "https://www.sailpoint.com/solutions/mitigate-cyber-risk",
+    "https://www.sailpoint.com/solutions/zero-trust",
+    "https://www.sailpoint.com/solutions/improve-it-efficiency",
+    "https://www.sailpoint.com/solutions/onboarding-offboarding",
+    "https://www.sailpoint.com/solutions/maintain-compliance",
+    "https://www.sailpoint.com/solutions/industries/higher-education",
+    "https://www.sailpoint.com/solutions/industries/financial-services",
+    "https://www.sailpoint.com/solutions/industries/government",
+    "https://www.sailpoint.com/solutions/industries/healthcare",
+    "https://www.sailpoint.com/solutions/industries/manufacturing",
+    "https://www.sailpoint.com/why-us",
+    "https://www.sailpoint.com/why-us/diversity-inclusion-belonging",
+    "https://www.sailpoint.com/why-us/leadership",
+    "https://www.sailpoint.com/why-us/welcoming-to-all"
+  ];
+
+
+  window.addEventListener("locationchange", function () {
+    url = location.href;
+    urlCheck(url);
+    if (document.querySelector('.SPZ_7003')) {
+      document.body.classList.remove("SPZ_7003");
+    }
+  });
+  var url = location.href;
+  urlCheck(url);
+
+  function urlCheck(url) {
+    const normalizeURL = url => url.replace(/\/$/, '');
+    const currentURL = normalizeURL(window.location.href.split('?')[0]);
+
+    if (urls.map(normalizeURL).includes(currentURL)) {
+      createTest();
+    } else if (currentURL === normalizeURL("https://www.sailpoint.com/contact-us")) {
+      contact();
+    } else {
+      removeTest();
+    }
+  }
+
+  // Generic Code
+  function waitForElm(selector) {
+    return new Promise(function (resolve) {
+      if (document.querySelector(selector)) {
+        return resolve(document.querySelector(selector));
+      }
+      const observer = new MutationObserver(function (mutations) {
+        if (document.querySelector(selector)) {
+          resolve(document.querySelector(selector));
+          observer.disconnect();
+        }
+      });
+      observer.observe(document, { attributes: true, childList: true, subtree: true, characterData: true });
+    });
+  }
+  
+  // Do not touch below hidden field code for any Experiment Start
+  function removeSpecificCookieValue(targetName, targetValue) {
+    ['HiddenFieldNameContact', 'HiddenFieldValueContact'].forEach((key, i) => {
+        var values = getCookie(key)?.split(',').filter(v => v !== (i ? targetValue : targetName)).join(',');
+        setCookie(key, values || '', 1);
+    });
+  }
+
+  function hiddenValue(currentHiddenFieldName, currentHiddenFieldValue) {
+    var ExistingHiddenFieldName = getCookie('HiddenFieldNameContact');
+    var ExistingHiddenFieldValue = getCookie('HiddenFieldValueContact');
+
+    if (!ExistingHiddenFieldName) {
+        setCookie('HiddenFieldNameContact', currentHiddenFieldName, 1);
+        setCookie('HiddenFieldValueContact', currentHiddenFieldValue, 1);
+    } else if (ExistingHiddenFieldName && !ExistingHiddenFieldName.includes(currentHiddenFieldName) && !ExistingHiddenFieldValue.includes(currentHiddenFieldValue)) {
+        setCookie('HiddenFieldNameContact', ExistingHiddenFieldName + ',' + currentHiddenFieldName, 1);
+        setCookie('HiddenFieldValueContact', ExistingHiddenFieldValue + ',' + currentHiddenFieldValue, 1);
+    }
+
+    setHiddenFieldValue();
+  }
+
+  function setCookie(name, value, days) {
+    var expires = "";
+    if (days) {
+      var date = new Date();
+      date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+      expires = "; expires=" + date.toUTCString();
+    }
+    document.cookie = name + "=" + (value || "") + expires + "; path=/; domain=sailpoint.com;";
+  }
+
+  function getCookie(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for (var i = 0; i < ca.length; i++) {
+      var c = ca[i];
+      while (c.charAt(0) == ' ') c = c.substring(1, c.length);
+      if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+    }
+    return null;
+  }
+
+  function setHiddenFieldValue() {
+    var spz_cro_Interval = setInterval(function () {
+        var intellimize1 = document.querySelector('form.mktoForm#mktoForm_1017 input[name="intellimize1"]');
+        if (intellimize1) {
+            clearInterval(spz_cro_Interval);
+            var ExistingHiddenFieldValue = getCookie('HiddenFieldValueContact');
+            //check if hidden field value is empty then only set the value else set the value with , seperated
+            if (intellimize1.value == '') {
+              intellimize1.value = ExistingHiddenFieldValue;
+            }
+            else {
+                if (!intellimize1.value.includes(ExistingHiddenFieldValue)) {
+                  intellimize1.value = intellimize1.value + ',' + ExistingHiddenFieldValue;
+                }
+            }
+        }
+    });
+  }
+
+   //click event listener
+    document.addEventListener('click', function (e) {
+        if (e.target.closest('#mktoForm_1017 .mktoButton')) {
+            //inject current time and date in EST timezone into .intellimize2 hidden field
+            var d = new Date();
+            var n = d.toLocaleString('en-US', { timeZone: 'America/New_York' });
+            var int2 = e.target.closest('.mktoForm').querySelector('input[name="intellimize2"]');
+            if (int2)
+                int2.value = n;
+        }
+    });
+  // Do not touch below hidden field code for any Experiment End
+})();
